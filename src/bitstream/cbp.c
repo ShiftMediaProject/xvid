41878ce86e04d27be8ec42672d2b9b4c3603a8b0
edgomez <edgomez@060a26f8-e6a2-0410-8959-474133d486e0>
2002-03-28 21:07:26 +0000

    Sorry, the previous patch was buggy. This one shoud fix the problem
    git-svn-id: http://svn.xvid.org/trunk/xvidcore@79 060a26f8-e6a2-0410-8959-474133d486e0

--- src/bitstream/cbp.c
+++ src/bitstream/cbp.c
@@ -12,19 +12,19 @@ uint32_t calc_cbp_c(const int16_t codes[6*64])
 	uint32_t i, j;
 	uint32_t cbp = 0;
 
-	for (i = 0; i < 6*64; i+=64)
+	for (i = 0; i < 6; i++)
 	{
 		for (j = 1; j < 61; j+=4)
 		{
-			if (codes[i + j    ]|codes[i + j + 1]|
-			    codes[i + j + 2]|codes[i + j + 3])
+			if (codes[i*64 + j    ]|codes[i*64 + j + 1]|
+			    codes[i*64 + j + 2]|codes[i*64 + j + 3])
 			{
 				cbp |= 1 << (5 - i);
 				break;
 			}
 		}
 
-		if(codes[i + j]|codes[i + j +1]|codes[i + j + 2])
+		if(codes[i*64 + j]|codes[i*64 + j + 1]|codes[i*64 + j + 2])
 			cbp |= 1 << (5 - i);
 
 	}
