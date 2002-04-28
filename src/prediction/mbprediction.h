4458ef8d6147e01398babfbae0d6a055cb05e6de
edgomez <edgomez@060a26f8-e6a2-0410-8959-474133d486e0>
2002-04-28 23:36:28 +0000

    Cosmetic, gpl header
    git-svn-id: http://svn.xvid.org/trunk/xvidcore@149 060a26f8-e6a2-0410-8959-474133d486e0

--- src/prediction/mbprediction.h
+++ src/prediction/mbprediction.h
@@ -1,3 +1,35 @@
+/**************************************************************************
+ *
+ *  XVID MPEG-4 VIDEO CODEC
+ *  -  MB prediction header file  -
+ *
+ *  This program is an implementation of a part of one or more MPEG-4
+ *  Video tools as specified in ISO/IEC 14496-2 standard.  Those intending
+ *  to use this software module in hardware or software products are
+ *  advised that its use may infringe existing patents or copyrights, and
+ *  any such use would be at such party's own risk.  The original
+ *  developer of this software module and his/her company, and subsequent
+ *  editors and their companies, will have no liability for use of this
+ *  software or modifications or derivatives thereof.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the xvid_free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the xvid_free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *  $Id: mbprediction.h,v 1.5 2002-04-28 23:36:28 edgomez Exp $
+ *
+ *************************************************************************/
+
 #ifndef _MBPREDICTION_H_
 #define _MBPREDICTION_H_
 
@@ -8,54 +40,56 @@
 #define MIN(X, Y) ((X)<(Y)?(X):(Y))
 #define MAX(X, Y) ((X)>(Y)?(X):(Y))
 
-// very large value
+/* very large value */
 #define MV_MAX_ERROR	(4096 * 256)
 
 #define MVequal(A,B) ( ((A).x)==((B).x) && ((A).y)==((B).y) )
 
-void MBPrediction(FRAMEINFO *frame,	 /* <-- the parameter for ACDC and MV prediction */
-		  uint32_t x_pos,		 /* <-- The x position of the MB to be searched */
-		  uint32_t y_pos, 		 /* <-- The y position of the MB to be searched */ 
-		  uint32_t x_dim,		 /* <-- Number of macroblocks in a row */
-		  int16_t *qcoeff 	 /* <-> The quantized DCT coefficients */ 
-    );
+void MBPrediction(
+	FRAMEINFO *frame, /* <-- The parameter for ACDC and MV prediction */
+	uint32_t x_pos,   /* <-- The x position of the MB to be searched  */
+	uint32_t y_pos,   /* <-- The y position of the MB to be searched  */
+	uint32_t x_dim,   /* <-- Number of macroblocks in a row           */
+	int16_t *qcoeff); /* <-> The quantized DCT coefficients           */ 
 
 void add_acdc(MACROBLOCK *pMB,
-				uint32_t block, 
-				int16_t dct_codes[64],
-				uint32_t iDcScaler,
-				int16_t predictors[8]);
+	      uint32_t block, 
+	      int16_t dct_codes[64],
+	      uint32_t iDcScaler,
+	      int16_t predictors[8]);
 
 
 void predict_acdc(MACROBLOCK *pMBs,
-				uint32_t x, uint32_t y,	uint32_t mb_width, 
-				uint32_t block, 
-				int16_t qcoeff[64],
-				uint32_t current_quant,
-				int32_t iDcScaler,
-				int16_t predictors[8]);
+		  uint32_t x,
+		  uint32_t y,
+		  uint32_t mb_width, 
+		  uint32_t block, 
+		  int16_t qcoeff[64],
+		  uint32_t current_quant,
+		  int32_t iDcScaler,
+		  int16_t predictors[8]);
 
 /* get_pmvdata returns the median predictor and nothing else */
 
 static __inline VECTOR get_pmv(const MACROBLOCK * const pMBs,
-							const uint32_t x, const uint32_t y,
-							const uint32_t x_dim,
-							const uint32_t block)
+			       const uint32_t x,
+			       const uint32_t y,
+			       const uint32_t x_dim,
+			       const uint32_t block)
 {
 
-    int xin1, xin2, xin3;
-    int yin1, yin2, yin3;
-    int vec1, vec2, vec3;
-    VECTOR lneigh,tneigh,trneigh; /* left neighbour, top neighbour, topright neighbour */
-    VECTOR median;
+	int xin1, xin2, xin3;
+	int yin1, yin2, yin3;
+	int vec1, vec2, vec3;
+	VECTOR lneigh,tneigh,trneigh; /* left neighbour, top neighbour, topright neighbour */
+	VECTOR median;
 
-    static VECTOR zeroMV = {0,0};
-    uint32_t index = x + y * x_dim;
-//    zeroMV.x = zeroMV.y = 0;
+	static VECTOR zeroMV = {0,0};
+	uint32_t index = x + y * x_dim;
 
-	// first row (special case)
-    if (y == 0 && (block == 0 || block == 1))
-    {
+	/* first row (special case) */
+	if (y == 0 && (block == 0 || block == 1))
+	{
 		if ((x == 0) && (block == 0))		// first column, first block
 		{ 
 			return zeroMV;
@@ -68,23 +102,23 @@ static __inline VECTOR get_pmv(const MACROBLOCK * const pMBs,
 		{
 			return pMBs[index-1].mvs[1];
 		}
-    }
+	}
 
 	/*
-		MODE_INTER, vm18 page 48
-		MODE_INTER4V vm18 page 51
+	 * MODE_INTER, vm18 page 48
+	 * MODE_INTER4V vm18 page 51
+	 *
+	 *   (x,y-1)      (x+1,y-1)
+	 *   [   |   ]    [   |   ]
+	 *   [ 2 | 3 ]    [ 2 |   ]
+	 *
+	 *   (x-1,y)       (x,y)        (x+1,y)
+	 *   [   | 1 ]    [ 0 | 1 ]    [ 0 |   ]
+	 *   [   | 3 ]    [ 2 | 3 ]    [   |   ]
+	 */
 
-					(x,y-1)		(x+1,y-1)
-					[   |   ]	[	|   ]
-					[ 2 | 3 ]	[ 2 |   ]
-
-		(x-1,y)		(x,y)		(x+1,y)
-		[   | 1 ]	[ 0 | 1 ]	[ 0 |   ]
-		[   | 3 ]	[ 2 | 3 ]	[	|   ]
-	*/
-
-    switch (block)
-    {
+	switch (block)
+	{
 	case 0:
 		xin1 = x - 1;	yin1 = y;	vec1 = 1;	/* left */
 		xin2 = x;	yin2 = y - 1;	vec2 = 2;	/* top */
@@ -94,17 +128,17 @@ static __inline VECTOR get_pmv(const MACROBLOCK * const pMBs,
 		xin1 = x;		yin1 = y;		vec1 = 0;	
 		xin2 = x;		yin2 = y - 1;   vec2 = 3;
 		xin3 = x + 1;	yin3 = y - 1;	vec3 = 2;
-	    break;
+		break;
 	case 2:
 		xin1 = x - 1;	yin1 = y;		vec1 = 3;
 		xin2 = x;		yin2 = y;		vec2 = 0;
 		xin3 = x;		yin3 = y;		vec3 = 1;
-	    break;
+		break;
 	default:
 		xin1 = x;		yin1 = y;		vec1 = 2;
 		xin2 = x;		yin2 = y;		vec2 = 0;
 		xin3 = x;		yin3 = y;		vec3 = 1;
-    }
+	}
 
 
 	if (xin1 < 0 || /* yin1 < 0  || */ xin1 >= (int32_t)x_dim)
@@ -134,7 +168,7 @@ static __inline VECTOR get_pmv(const MACROBLOCK * const pMBs,
 		trneigh = pMBs[xin3 + yin3 * x_dim].mvs[vec3];
 	}
 
-	// median,minimum
+	/* median,minimum */
 	
 	median.x = MIN(MAX(lneigh.x, tneigh.x), MIN(MAX(tneigh.x, trneigh.x), MAX(lneigh.x, trneigh.x)));
 	median.y = MIN(MAX(lneigh.y, tneigh.y), MIN(MAX(tneigh.y, trneigh.y), MAX(lneigh.y, trneigh.y)));
@@ -146,35 +180,37 @@ static __inline VECTOR get_pmv(const MACROBLOCK * const pMBs,
    instead of only Median MV */
 
 static __inline int get_pmvdata(const MACROBLOCK * const pMBs,
-							const uint32_t x, const uint32_t y,
-							const uint32_t x_dim,
-							const uint32_t block,
-							VECTOR * const pmv, 
-							int32_t * const psad)
+				const uint32_t x, const uint32_t y,
+				const uint32_t x_dim,
+				const uint32_t block,
+				VECTOR * const pmv, 
+				int32_t * const psad)
 {
-/* pmv are filled with: 
-	[0]: Median (or whatever is correct in a special case)
-	[1]: left neighbour
-	[2]: top neighbour, 
-	[3]: topright neighbour,
-   psad are filled with:
-	[0]: minimum of [1] to [3]
-	[1]: left neighbour's SAD	// [1] to [3] are actually not needed  
-	[2]: top neighbour's SAD, 
-	[3]: topright neighbour's SAD,
-*/
 
-    int xin1, xin2, xin3;
-    int yin1, yin2, yin3;
-    int vec1, vec2, vec3;
+	/*
+	 * pmv are filled with: 
+	 *  [0]: Median (or whatever is correct in a special case)
+	 *  [1]: left neighbour
+	 *  [2]: top neighbour
+	 *  [3]: topright neighbour
+	 * psad are filled with:
+	 *  [0]: minimum of [1] to [3]
+	 *  [1]: left neighbour's SAD (NB:[1] to [3] are actually not needed)  
+	 *  [2]: top neighbour's SAD
+	 *  [3]: topright neighbour's SAD
+	 */
 
-    static VECTOR zeroMV;
-    uint32_t index = x + y * x_dim;
-    zeroMV.x = zeroMV.y = 0;
+	int xin1, xin2, xin3;
+	int yin1, yin2, yin3;
+	int vec1, vec2, vec3;
+
+	static VECTOR zeroMV;
+	uint32_t index = x + y * x_dim;
+	zeroMV.x = zeroMV.y = 0;
 
 	// first row (special case)
-    if (y == 0 && (block == 0 || block == 1))
-    {
+	if (y == 0 && (block == 0 || block == 1))
+	{
 		if ((x == 0) && (block == 0))		// first column, first block
 		{ 
 			pmv[0] = pmv[1] = pmv[2] = pmv[3] = zeroMV;
@@ -197,43 +233,43 @@ static __inline int get_pmvdata(const MACROBLOCK * const pMBs,
 			psad[2] = psad[3] = MV_MAX_ERROR;
 			return 0;
 		}
-    }
+	}
 
 	/*
-		MODE_INTER, vm18 page 48
-		MODE_INTER4V vm18 page 51
+	 * MODE_INTER, vm18 page 48
+	 * MODE_INTER4V vm18 page 51
+	 *
+	 *  (x,y-1)      (x+1,y-1)
+	 *  [   |   ]    [   |   ]
+	 *  [ 2 | 3 ]    [ 2 |   ]
+	 *
+	 *  (x-1,y)      (x,y)        (x+1,y)
+	 *  [   | 1 ]    [ 0 | 1 ]    [ 0 |   ]
+	 *  [   | 3 ]    [ 2 | 3 ]    [   |   ]
+	 */
 
-					(x,y-1)		(x+1,y-1)
-					[   |   ]	[	|   ]
-					[ 2 | 3 ]	[ 2 |   ]
-
-		(x-1,y)		(x,y)		(x+1,y)
-		[   | 1 ]	[ 0 | 1 ]	[ 0 |   ]
-		[   | 3 ]	[ 2 | 3 ]	[	|   ]
-	*/
-
-    switch (block)
-    {
+	switch (block)
+	{
 	case 0:
-		xin1 = x - 1;	yin1 = y;	vec1 = 1;	/* left */
-		xin2 = x;	yin2 = y - 1;	vec2 = 2;	/* top */
-		xin3 = x + 1;	yin3 = y - 1;	vec3 = 2;	/* top right */
+		xin1 = x - 1; yin1 = y;     vec1 = 1; /* left */
+		xin2 = x;     yin2 = y - 1; vec2 = 2; /* top */
+		xin3 = x + 1; yin3 = y - 1; vec3 = 2; /* top right */
 		break;
 	case 1:
-		xin1 = x;		yin1 = y;		vec1 = 0;	
-		xin2 = x;		yin2 = y - 1;   vec2 = 3;
-		xin3 = x + 1;	yin3 = y - 1;	vec3 = 2;
-	    break;
+		xin1 = x;     yin1 = y;     vec1 = 0;	
+		xin2 = x;     yin2 = y - 1; vec2 = 3;
+		xin3 = x + 1; yin3 = y - 1; vec3 = 2;
+		break;
 	case 2:
-		xin1 = x - 1;	yin1 = y;		vec1 = 3;
-		xin2 = x;		yin2 = y;		vec2 = 0;
-		xin3 = x;		yin3 = y;		vec3 = 1;
-	    break;
+		xin1 = x - 1; yin1 = y; vec1 = 3;
+		xin2 = x;     yin2 = y; vec2 = 0;
+		xin3 = x;     yin3 = y; vec3 = 1;
+		break;
 	default:
-		xin1 = x;		yin1 = y;		vec1 = 2;
-		xin2 = x;		yin2 = y;		vec2 = 0;
-		xin3 = x;		yin3 = y;		vec3 = 1;
-    }
+		xin1 = x; yin1 = y; vec1 = 2;
+		xin2 = x; yin2 = y; vec2 = 0;
+		xin3 = x; yin3 = y; vec3 = 1;
+	}
 
 
 	if (xin1 < 0 || /* yin1 < 0  || */ xin1 >= (int32_t)x_dim)
@@ -271,15 +307,16 @@ static __inline int get_pmvdata(const MACROBLOCK * const pMBs,
 
 	if ( (MVequal(pmv[1],pmv[2])) && (MVequal(pmv[1],pmv[3])) )
 	{	pmv[0]=pmv[1];
-		psad[0]=psad[1];
-		return 1;
+	psad[0]=psad[1];
+	return 1;
 	}
 
-	// median,minimum
+	/* median,minimum */
 	
 	pmv[0].x = MIN(MAX(pmv[1].x, pmv[2].x), MIN(MAX(pmv[2].x, pmv[3].x), MAX(pmv[1].x, pmv[3].x)));
 	pmv[0].y = MIN(MAX(pmv[1].y, pmv[2].y), MIN(MAX(pmv[2].y, pmv[3].y), MAX(pmv[1].y, pmv[3].y)));
 	psad[0]=MIN(MIN(psad[1],psad[2]),psad[3]);
+
 	return 0;
 }
 
