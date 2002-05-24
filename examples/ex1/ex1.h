b478f477afe4816dc7189ecac904fe2bc625516b
chenm001 <chenm001@060a26f8-e6a2-0410-8959-474133d486e0>
2002-05-24 00:25:00 +0000

    example for use xvidcore to compress iamge
    git-svn-id: http://svn.xvid.org/trunk/xvidcore@182 060a26f8-e6a2-0410-8959-474133d486e0

---
+++ examples/ex1/ex1.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include <
+#include "xvid.h"
+
+#define	ERR_OK					(0)
+#define ERR_NO_OPT				(1)
+#define ERR_OPT_NOT_SUPPORT		(2)
+#define ERR_FILE				(3)
+#define ERR_VERSION				(4)
+#define ERR_MEMORY				(5)
+
+
+#define BUF_LEN					(352*288*3)
---
+++ examples/ex1/readme.txt
@@ -0,0 +1,6 @@
+This directory is a example for xvidcore API Version 2.1
+it example how to use the xvidcore to encode/decode image
+
+
+Author:
+    chenm001<chenm001@163.com>
