ca6916ffcd7fd948fa244ceccae94744b1a2d2f9
edgomez <edgomez@060a26f8-e6a2-0410-8959-474133d486e0>
2002-06-13 22:05:09 +0000

    (split)- Added legal header - Added comments about the functions
    git-svn-id: http://svn.xvid.org/trunk@204 060a26f8-e6a2-0410-8959-474133d486e0

--- src/utils/mem_align.c
+++ src/utils/mem_align.c
@@ -1,6 +1,63 @@
+/*****************************************************************************
+ *
+ *  XVID MPEG-4 VIDEO CODEC
+ *  - Aligned memory allocator -
+ *
+ *  This program is an implementation of a part of one or more MPEG-4
+ *  Video tools as specified in ISO/IEC 14496-2 standard.  Those intending
+ *  to use this software module in hardware or software products are
+ *  advised that its use may infringe existing patents or copyrights, and
+ *  any such use would be at such party's own risk.  The original
+ *  developer of this software module and his/her company, and subsequent
+ *  editors and their companies, will have no liability for use of this
+ *  software or modifications or derivatives thereof.
+ *
+ *  This program is free software ; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation ; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY ; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program ; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ ****************************************************************************/
+/*****************************************************************************
+ *
+ *  History
+ *
+ *  - Thu Jun 13 23:50:07 2002 Added legal header
+ *
+ *  $Id: mem_align.c,v 1.7 2002-06-13 22:05:09 edgomez Exp $
+ *
+ ****************************************************************************/
+
+
 #include <stdlib.h>
 #include <stdio.h>
 #include "mem_align.h"
+
+/*****************************************************************************
+ * xvid_malloc
+ *
+ * This function allocates 'size' bytes (usable by the user) on the heap and
+ * takes care of the requested 'alignment'.
+ * In order to align the allocated memory block, the xvid_malloc allocates
+ * 'size' bytes + 'alignment' bytes. So try to keep alignment very small
+ * when allocating small pieces of memory.
+ *
+ * NB : a block allocated by xvid_malloc _must_ be freed with xvid_free
+ *      (the libc free will return an error)
+ *
+ * Returned value : - NULL on error
+ *                  - Pointer to the allocated aligned block
+ *
+ ****************************************************************************/
 
 void *
 xvid_malloc(size_t size,
@@ -61,6 +118,16 @@ xvid_malloc(size_t size,
 	return NULL;
 
 }
+
+/*****************************************************************************
+ * xvid_free
+ *
+ * Free a previously 'xvid_malloc' allocated block. Does not free NULL
+ * references.
+ *
+ * Returned value : None.
+ *
+ ****************************************************************************/
 
 void
 xvid_free(void *mem_ptr)
