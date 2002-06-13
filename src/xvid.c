26306ec25470f9060368ea147e826e7d3eef37ae
edgomez <edgomez@060a26f8-e6a2-0410-8959-474133d486e0>
2002-06-13 21:35:01 +0000

    Cosmetic
    git-svn-id: http://svn.xvid.org/trunk/xvidcore@201 060a26f8-e6a2-0410-8959-474133d486e0

--- src/xvid.c
+++ src/xvid.c
@@ -1,43 +1,43 @@
 /*****************************************************************************
-*
-*  XVID MPEG-4 VIDEO CODEC
-*  - Native API implementation  -
-*
-*  This program is an implementation of a part of one or more MPEG-4
-*  Video tools as specified in ISO/IEC 14496-2 standard.  Those intending
-*  to use this software module in hardware or software products are
-*  advised that its use may infringe existing patents or copyrights, and
-*  any such use would be at such party's own risk.  The original
-*  developer of this software module and his/her company, and subsequent
-*  editors and their companies, will have no liability for use of this
-*  software or modifications or derivatives thereof.
-*
-*  This program is free software ; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation ; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  This program is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY ; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  You should have received a copy of the GNU General Public License
-*  along with this program ; if not, write to the Free Software
-*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
-*
-*****************************************************************************/
+ *
+ *  XVID MPEG-4 VIDEO CODEC
+ *  - Native API implementation  -
+ *
+ *  This program is an implementation of a part of one or more MPEG-4
+ *  Video tools as specified in ISO/IEC 14496-2 standard.  Those intending
+ *  to use this software module in hardware or software products are
+ *  advised that its use may infringe existing patents or copyrights, and
+ *  any such use would be at such party's own risk.  The original
+ *  developer of this software module and his/her company, and subsequent
+ *  editors and their companies, will have no liability for use of this
+ *  software or modifications or derivatives thereof.
+ *
+ *  This program is free software ; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation ; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY ; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program ; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ ****************************************************************************/
 /*****************************************************************************
-*
-*  History
-*
-*  - 17.03.2002	Added interpolate8x8_halfpel_hv_xmm
-*  - 22.12.2001  API change: added xvid_init() - Isibaar
-*  - 16.12.2001	inital version; (c)2001 peter ross <pross@cs.rmit.edu.au>
-*
-*  $Id: xvid.c,v 1.16 2002-06-13 13:18:57 edgomez Exp $
-*
-*****************************************************************************/
+ *
+ *  History
+ *
+ *  - 17.03.2002	Added interpolate8x8_halfpel_hv_xmm
+ *  - 22.12.2001  API change: added xvid_init() - Isibaar
+ *  - 16.12.2001	inital version; (c)2001 peter ross <pross@cs.rmit.edu.au>
+ *
+ *  $Id: xvid.c,v 1.17 2002-06-13 21:35:01 edgomez Exp $
+ *
+ ****************************************************************************/
 
 #include "xvid.h"
 #include "decoder.h"
