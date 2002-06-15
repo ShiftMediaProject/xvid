6a6a3a0855458064ea814c687d4df277c7902671
edgomez <edgomez@060a26f8-e6a2-0410-8959-474133d486e0>
2002-06-15 22:15:57 +0000

    - Added comments - Changed the FSF address
    git-svn-id: http://svn.xvid.org/trunk/xvidcore@212 060a26f8-e6a2-0410-8959-474133d486e0

--- src/utils/mem_transfer.c
+++ src/utils/mem_transfer.c
@@ -1,60 +1,81 @@
-/**************************************************************************
+/*****************************************************************************
  *
- *	XVID MPEG-4 VIDEO CODEC
- *	8bit<->16bit transfer
+ *  XVID MPEG-4 VIDEO CODEC
+ *  - 8bit<->16bit transfer  -
  *
- *	This program is an implementation of a part of one or more MPEG-4
- *	Video tools as specified in ISO/IEC 14496-2 standard.  Those intending
- *	to use this software module in hardware or software products are
- *	advised that its use may infringe existing patents or copyrights, and
- *	any such use would be at such party's own risk.  The original
- *	developer of this software module and his/her company, and subsequent
- *	editors and their companies, will have no liability for use of this
- *	software or modifications or derivatives thereof.
+ *  This program is free software ; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation ; either version 2 of the License, or
+ *  (at your option) any later version.
  *
- *	This program is free software; you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation; either version 2 of the License, or
- *	(at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY ; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- *	This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program ; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  *
- *	You should have received a copy of the GNU General Public License
- *	along with this program; if not, write to the Free Software
- *	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ ****************************************************************************/
+/*****************************************************************************
  *
- *************************************************************************/
+ *  History
+ *
+ *  - 14.06.2002 Changed legal header with the new FSF address
+ *	- 14.04.2002 added transfer_8to16sub2
+ *	- 07.01.2002 merge functions from compensate; rename functions
+ *	- 22.12.2001 transfer_8to8add16 limit fix
+ *	- 07.11.2001 initial version; (c)2001 peter ross <pross@cs.rmit.edu.au>
+ *
+ *  $Id: mem_transfer.c,v 1.4 2002-06-15 22:15:57 edgomez Exp $
+ *
+ ****************************************************************************/
 
-/**************************************************************************
- *
- *	History:
- *
- *	14.04.2002	added transfer_8to16sub2
- *	07.01.2002	merge functions from compensate; rename functions
- *	22.12.2001	transfer_8to8add16 limit fix
- *	07.11.2001	initial version; (c)2001 peter ross <pross@cs.rmit.edu.au>
+#include "../global.h"
+#include "mem_transfer.h"
 
- *************************************************************************/
+/* Function pointers - Initialized in the xvid.c module */
+
+TRANSFER_8TO16COPY_PTR transfer_8to16copy;
+TRANSFER_16TO8COPY_PTR transfer_16to8copy;
 
 TRANSFER_8TO16SUB_PTR  transfer_8to16sub;
 TRANSFER_8TO16SUB2_PTR transfer_8to16sub2;
 TRANSFER_16TO8ADD_PTR  transfer_16to8add;
 
-// function pointers
+TRANSFER8X8_COPY_PTR transfer8x8_copy;
+
 
 /*****************************************************************************
  *
-TRANSFER_8TO16SUB_PTR transfer_8to16sub;
+ * All these functions are used to transfer data from a 8 bit data array
  * to a 16 bit data array.
-TRANSFER_16TO8ADD_PTR transfer_16to8add;
+ *
  * This is typically used during motion compensation, that's why some
  * functions also do the addition/substraction of another buffer during the
  * so called  transfer.
  *
+ ****************************************************************************/
+
+/*
+ * SRC - the source buffer
+ * DST - the destination buffer
+ *
+ * Then the function does the 8->16 bit transfer and this serie of operations :
+ *
+ *    SRC (8bit)  = SRC
+ *    DST (16bit) = SRC
  */
+void
+transfer_8to16copy_c(int16_t * const dst,
+					 const uint8_t * const src,
+					 uint32_t stride)
+{
+	uint32_t i, j;
+
+	for (j = 0; j < 8; j++) {
+		for (i = 0; i < 8; i++) {
 			dst[j * 8 + i] = (int16_t) src[j * stride + i];
 		}
 	}
@@ -69,7 +90,15 @@ TRANSFER_16TO8ADD_PTR transfer_16to8add;
  *    SRC (16bit) = SRC
  *    DST (8bit)  = max(min(SRC, 255), 0)
  */
+void
+transfer_16to8copy_c(uint8_t * const dst,
+					 const int16_t * const src,
+					 uint32_t stride)
+{
+	uint32_t i, j;
 
+	for (j = 0; j < 8; j++) {
+		for (i = 0; i < 8; i++) {
 			int16_t pixel = src[j * 8 + i];
 
 			if (pixel < 0) {
@@ -95,9 +124,16 @@ TRANSFER_16TO8ADD_PTR transfer_16to8add;
  *    R   (8bit)  = R
  *    C   (8bit)  = R
  *    DCT (16bit) = C - R
-  perform motion compensation (and 8bit->16bit dct transfer)
-  ... with write back
-*/
+ */
+void
+transfer_8to16sub_c(int16_t * const dct,
+					uint8_t * const cur,
+					const uint8_t * ref,
+					const uint32_t stride)
+{
+	uint32_t i, j;
+
+	for (j = 0; j < 8; j++) {
 		for (i = 0; i < 8; i++) {
 			uint8_t c = cur[j * stride + i];
 			uint8_t r = ref[j * stride + i];
@@ -119,9 +155,18 @@ TRANSFER_16TO8ADD_PTR transfer_16to8add;
  *
  *    R1  (8bit) = R1
  *    R2  (8bit) = R2
-  performs bi motion compensation (and 8bit->16bit dct transfer)
-  .. but does not write back
-*/
+ *    C   (8bit) = C
+ *    DCT (16bit)= C - min((R1 + R2)/2, 255)
+ */
+void
+transfer_8to16sub2_c(int16_t * const dct,
+					 uint8_t * const cur,
+					 const uint8_t * ref1,
+					 const uint8_t * ref2,
+					 const uint32_t stride)
+{
+	uint32_t i, j;
+
 	for (j = 0; j < 8; j++) {
 		for (i = 0; i < 8; i++) {
 			uint8_t c = cur[j * stride + i];
@@ -146,6 +191,15 @@ TRANSFER_16TO8ADD_PTR transfer_16to8add;
  *    SRC (16bit) = SRC
  *    DST (8bit)  = max(min(DST+SRC, 255), 0)
  */
+void
+transfer_16to8add_c(uint8_t * const dst,
+					const int16_t * const src,
+					uint32_t stride)
+{
+	uint32_t i, j;
+
+	for (j = 0; j < 8; j++) {
+		for (i = 0; i < 8; i++) {
 			int16_t pixel = (int16_t) dst[j * stride + i] + src[j * 8 + i];
 
 			if (pixel < 0) {
@@ -167,9 +221,15 @@ TRANSFER_16TO8ADD_PTR transfer_16to8add;
  *    SRC (8bit) = SRC
  *    DST (8bit) = SRC
  */
+void
+transfer8x8_copy_c(uint8_t * const dst,
+				   const uint8_t * const src,
+				   const uint32_t stride)
+{
+	uint32_t i, j;
 
-
-
+	for (j = 0; j < 8; j++) {
+		for (i = 0; i < 8; i++) {
 			dst[j * stride + i] = src[j * stride + i];
 		}
 	}
