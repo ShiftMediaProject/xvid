530a49d16ca14ab8d08a75694f153f2f301e5f56
chl <chl@060a26f8-e6a2-0410-8959-474133d486e0>
2002-07-10 19:17:49 +0000

    (split)Step towards blocks based interpolation ME: get_ipmv/get_ipmvdata
    git-svn-id: http://svn.xvid.org/trunk@286 060a26f8-e6a2-0410-8959-474133d486e0

--- src/prediction/mbprediction.h
+++ src/prediction/mbprediction.h
@@ -26,7 +26,7 @@
  *  along with this program; if not, write to the xvid_free Software
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *  $Id: mbprediction.h,v 1.12 2002-07-03 12:32:50 suxen_drol Exp $
+ *  $Id: mbprediction.h,v 1.13 2002-07-10 19:17:49 chl Exp $
  *
  *************************************************************************/
 
@@ -83,6 +83,7 @@ void predict_acdc(MACROBLOCK * pMBs,
 				const int bound);
 
 
+#ifdef OLD_GETPMV
 /* get_pmvdata returns the median predictor and nothing else */
 
 static __inline VECTOR
@@ -369,6 +370,7 @@ get_pmvdata(const MACROBLOCK * const pMBs,
 	return 0;
 }
 
+#endif
 
 	/*
 	 * MODE_INTER, vm18 page 48
@@ -452,7 +454,7 @@ get_pmv2(const MACROBLOCK * const mbs,
         pmv[3] = zeroMV;
     }
 
-    /* if only one valid candidate preictor, the invalid candiates are set to the canidate */
+    /* if only one valid candidate predictor, the invalid candiates are set to the canidate */
 	if (num_cand != 1) {
 		/* set median */
    
@@ -482,6 +484,7 @@ get_pmv2(const MACROBLOCK * const mbs,
 	 *  [2]: top neighbour's SAD
 	 *  [3]: topright neighbour's SAD
 	 */
+	 
 static __inline int
 get_pmvdata2(const MACROBLOCK * const mbs,
          const int mb_width,
@@ -552,6 +555,209 @@ get_pmvdata2(const MACROBLOCK * const mbs,
         last_cand = 3;
         pmv[3] = mbs[rpos].mvs[rz];
         psad[3] = mbs[rpos].sad8[rz];
+    } else {
+        pmv[3] = zeroMV;
+		psad[3] = MV_MAX_ERROR;
+    }
+
+	/* original pmvdata() compatibility hack */
+	if (x == 0 && y == 0 && block == 0)
+	{
+		pmv[0] = pmv[1] = pmv[2] = pmv[3] = zeroMV;
+		psad[0] = 0;
+		psad[1] = psad[2] = psad[3] = MV_MAX_ERROR;
+		return 0;
+	}
+
+    /* if only one valid candidate preictor, the invalid candiates are set to the canidate */
+	if (num_cand == 1) {
+		pmv[0] = pmv[last_cand];
+		psad[0] = psad[last_cand];
+        // return MVequal(pmv[0], zeroMV); /* no point calculating median mv and minimum sad */
+		
+		/* original pmvdata() compatibility hack */
+		return y==0 && block <= 1 ? 0 : MVequal(pmv[0], zeroMV);
+	}
+
+	if ((MVequal(pmv[1], pmv[2])) && (MVequal(pmv[1], pmv[3]))) {
+		pmv[0] = pmv[1];
+		psad[0] = MIN(MIN(psad[1], psad[2]), psad[3]);
+		return 1;
+		/* compatibility patch */
+		//return y==0 && block <= 1 ? 0 : 1;
+	}
+
+	/* set median, minimum */
+
+	pmv[0].x =
+		MIN(MAX(pmv[1].x, pmv[2].x),
+			MIN(MAX(pmv[2].x, pmv[3].x), MAX(pmv[1].x, pmv[3].x)));
+	pmv[0].y =
+		MIN(MAX(pmv[1].y, pmv[2].y),
+			MIN(MAX(pmv[2].y, pmv[3].y), MAX(pmv[1].y, pmv[3].y)));
+
+	psad[0] = MIN(MIN(psad[1], psad[2]), psad[3]);
+
+   	return 0;
+}
+
+/* copies of get_pmv and get_pmvdata for prediction from integer search */
+
+static __inline VECTOR
+get_ipmv(const MACROBLOCK * const mbs,
+         const int mb_width,
+         const int bound,
+         const int x,
+         const int y,
+         const int block)
+{
+	static const VECTOR zeroMV = { 0, 0 };
+    
+	int lx, ly, lz;         /* left */
+	int tx, ty, tz;         /* top */
+	int rx, ry, rz;         /* top-right */
+	int lpos, tpos, rpos;
+	int num_cand, last_cand;
+
+	VECTOR pmv[4];	/* left neighbour, top neighbour, top-right neighbour */
+
+	switch (block) {
+	case 0:
+		lx = x - 1;	ly = y;		lz = 1;
+		tx = x;		ty = y - 1;	tz = 2;
+		rx = x + 1;	ry = y - 1;	rz = 2;
+		break;
+	case 1:
+		lx = x;		ly = y;		lz = 0;
+		tx = x;		ty = y - 1;	tz = 3;
+		rx = x + 1;	ry = y - 1;	rz = 2;
+		break;
+	case 2:
+		lx = x - 1;	ly = y;		lz = 3;
+		tx = x;		ty = y;		tz = 0;
+		rx = x;		ry = y;		rz = 1;
+		break;
+	default:
+		lx = x;		ly = y;		lz = 2;
+		tx = x;		ty = y;		tz = 0;
+		rx = x;		ry = y;		rz = 1;
+	}
+
+    lpos = lx + ly * mb_width;
+    rpos = rx + ry * mb_width;
+    tpos = tx + ty * mb_width;
+    num_cand = 0;
+
+    if (lpos >= bound && lx >= 0) {
+        num_cand++;
+        last_cand = 1;
+        pmv[1] = mbs[lpos].i_mvs[lz];
+    } else {
+        pmv[1] = zeroMV;
+    }
+
+    if (tpos >= bound) {
+        num_cand++;
+        last_cand = 2;
+        pmv[2] = mbs[tpos].i_mvs[tz];
+    } else {
+        pmv[2] = zeroMV;
+    }
+    
+    if (rpos >= bound && rx < mb_width) {
+        num_cand++;
+        last_cand = 3;
+        pmv[3] = mbs[rpos].i_mvs[rz];
+    } else {
+        pmv[3] = zeroMV;
+    }
+
+    /* if only one valid candidate predictor, the invalid candiates are set to the canidate */
+	if (num_cand != 1) {
+		/* set median */
+   
+   		pmv[0].x =
+			MIN(MAX(pmv[1].x, pmv[2].x),
+				MIN(MAX(pmv[2].x, pmv[3].x), MAX(pmv[1].x, pmv[3].x)));
+		pmv[0].y =
+			MIN(MAX(pmv[1].y, pmv[2].y),
+				MIN(MAX(pmv[2].y, pmv[3].y), MAX(pmv[1].y, pmv[3].y)));
+		return pmv[0];
+	 }
+
+	 return pmv[last_cand];  /* no point calculating median mv */
+}
+
+static __inline int
+get_ipmvdata(const MACROBLOCK * const mbs,
+         const int mb_width,
+         const int bound,
+         const int x,
+         const int y,
+         const int block,
+		 VECTOR * const pmv,
+		 int32_t * const psad)
+{
+	static const VECTOR zeroMV = { 0, 0 };
+    
+    int lx, ly, lz;         /* left */
+    int tx, ty, tz;         /* top */
+    int rx, ry, rz;         /* top-right */
+    int lpos, tpos, rpos;
+    int num_cand, last_cand;
+
+	switch (block) {
+	case 0:
+		lx = x - 1;	ly = y;		lz = 1;
+		tx = x;		ty = y - 1;	tz = 2;
+		rx = x + 1;	ry = y - 1;	rz = 2;
+		break;
+	case 1:
+		lx = x;		ly = y;		lz = 0;
+		tx = x;		ty = y - 1;	tz = 3;
+		rx = x + 1;	ry = y - 1;	rz = 2;
+		break;
+	case 2:
+		lx = x - 1;	ly = y;		lz = 3;
+		tx = x;		ty = y;		tz = 0;
+		rx = x;		ry = y;		rz = 1;
+		break;
+	default:
+		lx = x;		ly = y;		lz = 2;
+		tx = x;		ty = y;		tz = 0;
+		rx = x;		ry = y;		rz = 1;
+	}
+
+    lpos = lx + ly * mb_width;
+    rpos = rx + ry * mb_width;
+    tpos = tx + ty * mb_width;
+    num_cand = 0;
+
+    if (lpos >= bound && lx >= 0) {
+        num_cand++;
+        last_cand = 1;
+        pmv[1] = mbs[lpos].i_mvs[lz];
+		psad[1] = mbs[lpos].i_sad8[lz];
+    } else {
+        pmv[1] = zeroMV;
+		psad[1] = MV_MAX_ERROR;
+    }
+
+    if (tpos >= bound) {
+        num_cand++;
+        last_cand = 2;
+        pmv[2]= mbs[tpos].i_mvs[tz];
+        psad[2] = mbs[tpos].i_sad8[tz];
+    } else {
+        pmv[2] = zeroMV;
+		psad[2] = MV_MAX_ERROR;
+    }
+    
+    if (rpos >= bound && rx < mb_width) {
+        num_cand++;
+        last_cand = 3;
+        pmv[3] = mbs[rpos].i_mvs[rz];
+        psad[3] = mbs[rpos].i_sad8[rz];
     } else {
         pmv[3] = zeroMV;
 		psad[3] = MV_MAX_ERROR;
