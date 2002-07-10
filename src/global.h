f5c292779402a36e70440758289890f7767c05d5
chl <chl@060a26f8-e6a2-0410-8959-474133d486e0>
2002-07-10 19:29:30 +0000

    Minor bugfix.
    git-svn-id: http://svn.xvid.org/trunk/xvidcore@287 060a26f8-e6a2-0410-8959-474133d486e0

--- src/global.h
+++ src/global.h
@@ -78,10 +78,10 @@ typedef struct
 	// backup of last integer ME vectors/sad
 	
 	VECTOR i_mv16;
-	VECTOR i_pmvs[4];
+	VECTOR i_mvs[4];
 
-	int32_t i_sad8[4];			// SAD values for inter4v-VECTORs
-	int32_t i_sad16;				// SAD value for inter-VECTOR
+	int32_t i_sad8[4];	// SAD values for inter4v-VECTORs
+	int32_t i_sad16;	// SAD value for inter-VECTOR
 
 }
 MACROBLOCK;
