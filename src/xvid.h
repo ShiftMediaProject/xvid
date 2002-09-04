8193a1ffed36362b98877722d9c0188ab924e0e5
edgomez <edgomez@060a26f8-e6a2-0410-8959-474133d486e0>
2002-09-04 22:12:21 +0000

    (split)- Fixed header to mention possible patent infringement - Removed History
    git-svn-id: http://svn.xvid.org/trunk@411 060a26f8-e6a2-0410-8959-474133d486e0

--- src/xvid.h
+++ src/xvid.h
@@ -1,37 +1,34 @@
 /*****************************************************************************
-*
-*  XVID MPEG-4 VIDEO CODEC
-*  - XviD Main header file -
-*
-*  This program is free software ; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation ; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  This program is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY ; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  You should have received a copy of the GNU General Public License
-*  along with this program ; if not, write to the Free Software
-*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
-*
-*****************************************************************************/
-/*****************************************************************************
-*
-*  History
-*
-*  - 2002/06/13 Added legal header, ANSI C comment style (only for this header
-*               as it can be included in a ANSI C project).
-*
-*               ToDo ? : when BFRAMES is defined, the API_VERSION should not
-*                        be the same (3.0 ?)
-*
-*  $Id: xvid.h,v 1.17 2002-07-31 18:10:34 chl Exp $
-*
-*****************************************************************************/
-
+ *
+ *  XVID MPEG-4 VIDEO CODEC
+ *  - XviD Main header file -
+ *
+ *  This program is an implementation of a part of one or more MPEG-4
+ *  Video tools as specified in ISO/IEC 14496-2 standard.  Those intending
+ *  to use this software module in hardware or software products are
+ *  advised that its use may infringe existing patents or copyrights, and
+ *  any such use would be at such party's own risk.  The original
+ *  developer of this software module and his/her company, and subsequent
+ *  editors and their companies, will have no liability for use of this
+ *  software or modifications or derivatives thereof.
+ *
+ *  This program is free software ; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation ; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY ; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program ; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ * $Id: xvid.h,v 1.18 2002-09-04 22:12:21 edgomez Exp $
+ *
+ *****************************************************************************/
 
 #ifndef _XVID_H_
 #define _XVID_H_
