3c73e691073ac3c6ee0283e5ee818e82d495e138
edgomez <edgomez@060a26f8-e6a2-0410-8959-474133d486e0>
2002-09-05 20:37:40 +0000

    Added legal header
    git-svn-id: http://svn.xvid.org/trunk/xvidcore@415 060a26f8-e6a2-0410-8959-474133d486e0

--- src/utils/ratecontrol.h
+++ src/utils/ratecontrol.h
@@ -1,3 +1,37 @@
+/*****************************************************************************
+ *
+ *  XVID MPEG-4 VIDEO CODEC
+ *  - 1 Pass rate controler header file -
+ *
+ *  Copyright(C) 2002 Michael Militzer
+ *
+ *  This program is an implementation of a part of one or more MPEG-4
+ *  Video tools as specified in ISO/IEC 14496-2 standard.  Those intending
+ *  to use this software module in hardware or software products are
+ *  advised that its use may infringe existing patents or copyrights, and
+ *  any such use would be at such party's own risk.  The original
+ *  developer of this software module and his/her company, and subsequent
+ *  editors and their companies, will have no liability for use of this
+ *  software or modifications or derivatives thereof.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ * $Id: ratecontrol.h,v 1.6 2002-09-05 20:37:40 edgomez Exp $
+ *
+ ****************************************************************************/
+
 #ifndef _RATECONTROL_H_
 #define _RATECONTROL_H_
 
@@ -48,4 +82,4 @@ void RateControlUpdate(RateControl * rate_control,
 					   int frame_size,
 					   int keyframe);
 
-#endif							/* _RATECONTROL_H_ */
+#endif /* _RATECONTROL_H_ */
