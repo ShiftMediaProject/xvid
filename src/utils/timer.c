3435e0b40a7fb430b561b6bf5f49f8b517a49a43
edgomez <edgomez@060a26f8-e6a2-0410-8959-474133d486e0>
2002-09-05 20:36:01 +0000

    (split)- Added information about possible patent infringements - Added NotaBene about the non thread safe state of the code
    git-svn-id: http://svn.xvid.org/trunk@414 060a26f8-e6a2-0410-8959-474133d486e0

--- src/utils/timer.c
+++ src/utils/timer.c
@@ -1,50 +1,44 @@
- /******************************************************************************
-  *                                                                            *
-  *  This file is part of XviD, a free MPEG-4 video encoder/decoder            *
-  *                                                                            *
-  *  XviD is free software; you can redistribute it and/or modify it           *
-  *  under the terms of the GNU General Public License as published by         *
-  *  the Free Software Foundation; either version 2 of the License, or         *
-  *  (at your option) any later version.                                       *
-  *                                                                            *
-  *  XviD is distributed in the hope that it will be useful, but               *
-  *  WITHOUT ANY WARRANTY; without even the implied warranty of                *
-  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
-  *  GNU General Public License for more details.                              *
-  *                                                                            *
-  *  You should have received a copy of the GNU General Public License         *
-  *  along with this program; if not, write to the Free Software               *
-  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA  *
-  *                                                                            *
-  ******************************************************************************/
-
- /******************************************************************************
-  *                                                                            *
-  *  timer.c, some timing functions                                            *
-  *                                                                            *
-  *  Copyright (C) 2001 - Michael Militzer <isibaar@xvid.org>                  *
-  *                                                                            *
-  *  For more information visit the XviD homepage: http://www.xvid.org         *
-  *                                                                            *
-  ******************************************************************************/
-
- /******************************************************************************
-  *                                                                            *
-  *  Revision history:                                                         *
-  *       
-  *  26.03.2002 interlacing timer added
-  *  21.12.2001 edges error fixed
-  *  17.11.2001 small clean up (Isibaar)                                       *
-  *  13.11.2001	inlined rdtsc call and moved to portab.h (Isibaar)             *
-  *  02.11.2001 initial version (Isibaar)                                      *
-  *                                                                            *
-  ******************************************************************************/
+/*****************************************************************************
+ *
+ *  XVID MPEG-4 VIDEO CODEC
+ *  - Some timing functions to profile the library -
+ *
+ *  NB : not thread safe and only for debug purposes.
+ *
+ *  Copyright(C) 2002 Michael Militzer
+ *
+ *  This program is an implementation of a part of one or more MPEG-4
+ *  Video tools as specified in ISO/IEC 14496-2 standard.  Those intending
+ *  to use this software module in hardware or software products are
+ *  advised that its use may infringe existing patents or copyrights, and
+ *  any such use would be at such party's own risk.  The original
+ *  developer of this software module and his/her company, and subsequent
+ *  editors and their companies, will have no liability for use of this
+ *  software or modifications or derivatives thereof.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ * $Id: timer.c,v 1.4 2002-09-05 20:36:01 edgomez Exp $
+ *
+ ****************************************************************************/
 
 #include <stdio.h>
 #include <time.h>
 #include "timer.h"
 
-#if defined(_PROFILING_)
+#ifdef _PROFILING_
 
 struct ts
 {
