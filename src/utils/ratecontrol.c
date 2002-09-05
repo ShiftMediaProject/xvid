15a881eb80eacfb19b522cb7f94e61e5328858c5
edgomez <edgomez@060a26f8-e6a2-0410-8959-474133d486e0>
2002-09-05 21:23:03 +0000

    Added patent infringement notice
    git-svn-id: http://svn.xvid.org/trunk/xvidcore@423 060a26f8-e6a2-0410-8959-474133d486e0

--- src/utils/ratecontrol.c
+++ src/utils/ratecontrol.c
@@ -3,35 +3,36 @@
  *  XVID MPEG-4 VIDEO CODEC
  *  - Rate Controler module -
  *
- *  This program is free software ; you can redistribute it and/or modify
+ *  Copyright(C) 2002 Michael Militzer
+ *
+ *  This program is an implementation of a part of one or more MPEG-4
+ *  Video tools as specified in ISO/IEC 14496-2 standard.  Those intending
+ *  to use this software module in hardware or software products are
+ *  advised that its use may infringe existing patents or copyrights, and
+ *  any such use would be at such party's own risk.  The original
+ *  developer of this software module and his/her company, and subsequent
+ *  editors and their companies, will have no liability for use of this
+ *  software or modifications or derivatives thereof.
+ *
+ *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation ; either version 2 of the License, or
+ *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY ; without even the implied warranty of
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with this program ; if not, write to the Free Software
+ *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  *
- ****************************************************************************/
-/*****************************************************************************
- *
- *  History
- *
- *  - Mon Jun 17 13:04:15 2002 Added legal header
- *
- *  $Id: ratecontrol.c,v 1.13 2002-06-17 12:23:32 edgomez Exp $
+ * $Id: ratecontrol.c,v 1.14 2002-09-05 21:23:03 edgomez Exp $
  *
  ****************************************************************************/
 
-
-/* For the fabs function */
 #include <math.h>
-
 #include "ratecontrol.h"
 
 /*****************************************************************************
