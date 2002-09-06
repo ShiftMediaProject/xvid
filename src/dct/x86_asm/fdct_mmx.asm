12d5402faaf37ed4efd45373a224a0fb5b7b61b5
chl <chl@060a26f8-e6a2-0410-8959-474133d486e0>
2002-09-06 17:30:31 +0000

    (split)- Update license/copyright header - Removed history header
    git-svn-id: http://svn.xvid.org/trunk@432 060a26f8-e6a2-0410-8959-474133d486e0

--- src/dct/x86_asm/fdct_mmx.asm
+++ src/dct/x86_asm/fdct_mmx.asm
@@ -1,51 +1,43 @@
-;/******************************************************************************
-; *                                                                            *
-; *  This file is part of XviD, a free MPEG-4 video encoder/decoder            *
-; *                                                                            *
-; *  This program is free software; you can redistribute it and/or modify it   *
-; *  under the terms of the GNU General Public License as published by         *
-; *  the Free Software Foundation; either version 2 of the License, or         *
-; *  (at your option) any later version.                                       *
-; *                                                                            *
-; *  This program is distributed in the hope that it will be useful, but       *
-; *  WITHOUT ANY WARRANTY; without even the implied warranty of                *
-; *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
-; *  GNU General Public License for more details.                              *
-; *                                                                            *
-; *  You should have received a copy of the GNU General Public License         *
-; *  along with this program; if not, write to the Free Software               *
-; *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA  *
-; *                                                                            *
-; ******************************************************************************/
-;
-;/******************************************************************************
-; *                                                                            *
-; *  fdct_mmx.asm, MMX optimized forward DCT                                   *
-; *                                                                            *
-; *  Initial, but incomplete version provided by Intel at AppNote AP-922       *
-; *  http://developer.intel.com/vtune/cbts/strmsimd/922down.htm                *
-; *  Copyright (C) 1999 Intel Corporation,                                     *
-; *                                                                            *
-; *  completed and corrected in fdctmm32.c/fdctmm32.doc,                       *
-; *  http://members.tripod.com/~liaor                                          *
-; *  Copyright (C) 2000 - Royce Shih-Wea Liao <liaor@iname.com>,               *
-; *                                                                            *
-; *  ported to NASM and some minor changes                                     * 
-; *  Copyright (C) 2001 - Michael Militzer <isibaar@xvid.org>                  *
-; *                                                                            *
-; *  For more information visit the XviD homepage: http://www.xvid.org         *
-; *                                                                            *
-; ******************************************************************************/
-;
-;/******************************************************************************
-; *                                                                            *
-; *  Revision history:                                                         *
-; *                                                                            *
-; *  04.11.2001 loop unrolled (Isibaar) 									   *
-; *  02.11.2001 initial version  (Isibaar)                                     *
-; *                                                                            *
-; ******************************************************************************/
-
+;/*****************************************************************************
+; *
+; *  XVID MPEG-4 VIDEO CODEC
+; *  mmx version - fast dicrete cosine transformation 
+; *
+; *  Initial, but incomplete version provided by Intel at AppNote AP-922       
+; *  http://developer.intel.com/vtune/cbts/strmsimd/922down.htm                
+; *  Copyright (C) 1999 Intel Corporation,                                     
+; *                                                                            
+; *  completed and corrected in fdctmm32.c/fdctmm32.doc,                       
+; *  http://members.tripod.com/~liaor                                          
+; *  Copyright (C) 2000 - Royce Shih-Wea Liao <liaor@iname.com>,               
+; *                                                                            
+; *  ported to NASM and some minor changes                                      
+; *  Copyright (C) 2001 - Michael Militzer <isibaar@xvid.org>                  
+; *                                                                            
+; *  This program is an implementation of a part of one or more MPEG-4
+; *  Video tools as specified in ISO/IEC 14496-2 standard.  Those intending
+; *  to use this software module in hardware or software products are
+; *  advised that its use may infringe existing patents or copyrights, and
+; *  any such use would be at such party's own risk.  The original
+; *  developer of this software module and his/her company, and subsequent
+; *  editors and their companies, will have no liability for use of this
+; *  software or modifications or derivatives thereof.
+; *
+; *  This program is free software; you can redistribute it and/or modify
+; *  it under the terms of the GNU General Public License as published by
+; *  the Free Software Foundation; either version 2 of the License, or
+; *  (at your option) any later version.
+; *
+; *  This program is distributed in the hope that it will be useful,
+; *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+; *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; *  GNU General Public License for more details.
+; *
+; *  You should have received a copy of the GNU General Public License
+; *  along with this program; if not, write to the Free Software
+; *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+; *
+; ****************************************************************************/
 
 BITS 32
 
