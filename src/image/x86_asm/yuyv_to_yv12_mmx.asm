6fab8591c6a53b1c0e2089da28784acd3dc416a2
chl <chl@060a26f8-e6a2-0410-8959-474133d486e0>
2002-09-06 17:48:58 +0000

    - Update license/copyright header - Removed history header
    git-svn-id: http://svn.xvid.org/trunk/xvidcore@434 060a26f8-e6a2-0410-8959-474133d486e0

--- src/image/x86_asm/yuyv_to_yv12_mmx.asm
+++ src/image/x86_asm/yuyv_to_yv12_mmx.asm
@@ -1,31 +1,34 @@
-;/**************************************************************************
+;/*****************************************************************************
 ; *
-; *	XVID MPEG-4 VIDEO CODEC
-; *	mmx yuyv/uyvy to yuv planar
+; *  XVID MPEG-4 VIDEO CODEC
+; *	 mmx yuyv/uyvy to yuv planar conversion 
 ; *
-; *	This program is free software; you can redistribute it and/or modify
-; *	it under the terms of the GNU General Public License as published by
-; *	the Free Software Foundation; either version 2 of the License, or
-; *	(at your option) any later version.
+; *  Copyright(C) 2002 Peter Ross <pross@xvid.org>
 ; *
-; *	This program is distributed in the hope that it will be useful,
-; *	but WITHOUT ANY WARRANTY; without even the implied warranty of
-; *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-; *	GNU General Public License for more details.
+; *  This program is an implementation of a part of one or more MPEG-4
+; *  Video tools as specified in ISO/IEC 14496-2 standard.  Those intending
+; *  to use this software module in hardware or software products are
+; *  advised that its use may infringe existing patents or copyrights, and
+; *  any such use would be at such party's own risk.  The original
+; *  developer of this software module and his/her company, and subsequent
+; *  editors and their companies, will have no liability for use of this
+; *  software or modifications or derivatives thereof.
 ; *
-; *	You should have received a copy of the GNU General Public License
-; *	along with this program; if not, write to the Free Software
-; *	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+; *  This program is free software; you can redistribute it and/or modify
+; *  it under the terms of the GNU General Public License as published by
+; *  the Free Software Foundation; either version 2 of the License, or
+; *  (at your option) any later version.
 ; *
-; *************************************************************************/
-
-;/**************************************************************************
+; *  This program is distributed in the hope that it will be useful,
+; *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+; *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; *  GNU General Public License for more details.
 ; *
-; *	History:
+; *  You should have received a copy of the GNU General Public License
+; *  along with this program; if not, write to the Free Software
+; *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 ; *
-; *	30.11.2001	initial version; (c)2001 peter ross <pross@cs.rmit.edu.au>
-; *
-; *************************************************************************/
+; ****************************************************************************/
 
 bits 32
 
