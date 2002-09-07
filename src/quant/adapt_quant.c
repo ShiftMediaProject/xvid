81150a10c9806f627bbf7d3417f3751d0bfc7b95
edgomez <edgomez@060a26f8-e6a2-0410-8959-474133d486e0>
2002-09-07 12:51:38 +0000

    (split)- Added Legal header - Cosmetic - Added Copyrigths
    git-svn-id: http://svn.xvid.org/trunk@443 060a26f8-e6a2-0410-8959-474133d486e0

--- src/quant/adapt_quant.c
+++ src/quant/adapt_quant.c
@@ -1,3 +1,37 @@
+/*****************************************************************************
+ *
+ *  XVID MPEG-4 VIDEO CODEC
+ *  - Adaptive quantization functions -
+ *
+ *  Copyright(C) 2002 Peter Ross
+ *
+ *  This program is an implementation of a part of one or more MPEG-4
+ *  Video tools as specified in ISO/IEC 14496-2 standard.  Those intending
+ *  to use this software module in hardware or software products are
+ *  advised that its use may infringe existing patents or copyrights, and
+ *  any such use would be at such party's own risk.  The original
+ *  developer of this software module and his/her company, and subsequent
+ *  editors and their companies, will have no liability for use of this
+ *  software or modifications or derivatives thereof.
+ *
+ *  This program is free software ; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation ; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY ; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program ; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ *  $Id: adapt_quant.c,v 1.7 2002-09-07 12:51:38 edgomez Exp $
+ *
+ ****************************************************************************/
+
 #include "../portab.h"
 #include "adapt_quant.h"
 
@@ -5,6 +39,10 @@
 
 #define MAX(a,b)      (((a) > (b)) ? (a) : (b))
 #define RDIFF(a,b)    ((int)(a+0.5)-(int)(b+0.5))
+
+/*****************************************************************************
+ * Functions
+ ****************************************************************************/
 
 int
 normalize_quantizer_field(float *in,
--- src/quant/quant_mpeg4.h
+++ src/quant/quant_mpeg4.h
@@ -1,9 +1,47 @@
+/*****************************************************************************
+ *
+ *  XVID MPEG-4 VIDEO CODEC
+ *  - Mpeg4 quantization/dequantization header -
+ *
+ *  Copyright(C) 2002 Peter Ross
+ *
+ *  This program is an implementation of a part of one or more MPEG-4
+ *  Video tools as specified in ISO/IEC 14496-2 standard.  Those intending
+ *  to use this software module in hardware or software products are
+ *  advised that its use may infringe existing patents or copyrights, and
+ *  any such use would be at such party's own risk.  The original
+ *  developer of this software module and his/her company, and subsequent
+ *  editors and their companies, will have no liability for use of this
+ *  software or modifications or derivatives thereof.
+ *
+ *  This program is free software ; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation ; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY ; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program ; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ *  $Id: quant_mpeg4.h,v 1.4 2002-09-07 12:51:38 edgomez Exp $
+ *
+ ****************************************************************************/
+
 #ifndef _QUANT_MPEG4_H_
 #define _QUANT_MPEG4_H_
 
 #include "../portab.h"
 
-// intra
+/*****************************************************************************
+ * Prototypes
+ ****************************************************************************/
+
+/* intra */
 typedef void (quant_intraFunc) (int16_t * coeff,
 								const int16_t * data,
 								const uint32_t quant,
@@ -20,7 +58,7 @@ quant_intraFunc quant4_intra_mmx;
 quant_intraFunc dequant4_intra_c;
 quant_intraFunc dequant4_intra_mmx;
 
-// inter_quant
+/* inter_quant */
 typedef uint32_t(quant_interFunc) (int16_t * coeff,
 								   const int16_t * data,
 								   const uint32_t quant);
@@ -32,7 +70,7 @@ extern quant_interFuncPtr quant4_inter;
 quant_interFunc quant4_inter_c;
 quant_interFunc quant4_inter_mmx;
 
-//inter_dequant
+/* inter_dequant */
 typedef void (dequant_interFunc) (int16_t * coeff,
 								  const int16_t * data,
 								  const uint32_t quant);
@@ -44,4 +82,4 @@ extern dequant_interFuncPtr dequant4_inter;
 dequant_interFunc dequant4_inter_c;
 dequant_interFunc dequant4_inter_mmx;
 
-#endif							/* _QUANT_MPEG4_H_ */
+#endif /* _QUANT_MPEG4_H_ */
