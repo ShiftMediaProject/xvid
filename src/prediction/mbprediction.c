f62455a14785a35c27f33d442c888280e22caf08
edgomez <edgomez@060a26f8-e6a2-0410-8959-474133d486e0>
2002-09-07 13:43:00 +0000

    (split)- Added headers - Added copyrights - Removed old getpmv functions
    git-svn-id: http://svn.xvid.org/trunk@445 060a26f8-e6a2-0410-8959-474133d486e0

--- src/prediction/mbprediction.c
+++ src/prediction/mbprediction.c
@@ -1,54 +1,37 @@
- /******************************************************************************
-  *                                                                            *
-  *  This file is part of XviD, a free MPEG-4 video encoder/decoder            *
-  *                                                                            *
-  *  XviD is an implementation of a part of one or more MPEG-4 Video tools     *
-  *  as specified in ISO/IEC 14496-2 standard.  Those intending to use this    *
-  *  software module in hardware or software products are advised that its     *
-  *  use may infringe existing patents or copyrights, and any such use         *
-  *  would be at such party's own risk.  The original developer of this        *
-  *  software module and his/her company, and subsequent editors and their     *
-  *  companies, will have no liability for use of this software or             *
-  *  modifications or derivatives thereof.                                     *
-  *                                                                            *
-  *  XviD is free software; you can redistribute it and/or modify it           *
-  *  under the terms of the GNU General Public License as published by         *
-  *  the Free Software Foundation; either version 2 of the License, or         *
-  *  (at your option) any later version.                                       *
-  *                                                                            *
-  *  XviD is distributed in the hope that it will be useful, but               *
-  *  WITHOUT ANY WARRANTY; without even the implied warranty of                *
-  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
-  *  GNU General Public License for more details.                              *
-  *                                                                            *
-  *  You should have received a copy of the GNU General Public License         *
-  *  along with this program; if not, write to the Free Software               *
-  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA  *
-  *                                                                            *
-  ******************************************************************************/
-
- /******************************************************************************
-  *                                                                            *
-  *  mbprediction.c                                                            *
-  *                                                                            *
-  *  Copyright (C) 2001 - Michael Militzer <isibaar@xvid.org>                  *
-  *  Copyright (C) 2001 - Peter Ross <pross@cs.rmit.edu.au>                    *
-  *                                                                            *
-  *  For more information visit the XviD homepage: http://www.xvid.org         *
-  *                                                                            *
-  ******************************************************************************/
-
- /******************************************************************************
-  *                                                                            *
-  *  Revision history:                                                         *
-  *                                                                            *
-  *  29.06.2002 predict_acdc() bounding                                        *
-  *  12.12.2001 improved calc_acdc_prediction; removed need for memcpy         *
-  *  15.12.2001 moved pmv displacement to motion estimation                    *
-  *  30.11.2001	mmx cbp support                                                *
-  *  17.11.2001 initial version                                                *
-  *                                                                            *
-  ******************************************************************************/
+/*****************************************************************************
+ *
+ *  XVID MPEG-4 VIDEO CODEC
+ *  - Prediction functions -
+ *
+ *  Copyright(C) 2001-2002 - Michael Militzer <isibaar@xvid.org>
+ *  Copyright(C) 2001-2002 - Peter Ross <pross@cs.rmit.edu.au>
+ *
+ *  This program is an implementation of a part of one or more MPEG-4
+ *  Video tools as specified in ISO/IEC 14496-2 standard.  Those intending
+ *  to use this software module in hardware or software products are
+ *  advised that its use may infringe existing patents or copyrights, and
+ *  any such use would be at such party's own risk.  The original
+ *  developer of this software module and his/her company, and subsequent
+ *  editors and their companies, will have no liability for use of this
+ *  software or modifications or derivatives thereof.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ * $Id: mbprediction.c,v 1.8 2002-09-07 13:43:00 edgomez Exp $
+ *
+ ****************************************************************************/
 
 #include "../encoder.h"
 #include "mbprediction.h"
@@ -60,6 +43,10 @@
 #define DIV_DIV(A,B)    ( (A) > 0 ? ((A)+((B)>>1))/(B) : ((A)-((B)>>1))/(B) )
 
 
+/*****************************************************************************
+ * Local inlined function
+ ****************************************************************************/
+
 static int __inline
 rescale(int predict_quant,
 		int current_quant,
@@ -70,11 +57,20 @@ rescale(int predict_quant,
 }
 
 
+/*****************************************************************************
+ * Local data
+ ****************************************************************************/
+
 static const int16_t default_acdc_values[15] = {
 	1024,
 	0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0, 0, 0
 };
+
+
+/*****************************************************************************
+ * Functions
+ ****************************************************************************/
 
 
 /*	get dc/ac prediction direction for a single block and place
