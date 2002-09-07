58aed21b16bbbd22385afd6fe4ab03b040716c50
edgomez <edgomez@060a26f8-e6a2-0410-8959-474133d486e0>
2002-09-07 10:26:10 +0000

    - Legal header - Copyright
    git-svn-id: http://svn.xvid.org/trunk/xvidcore@440 060a26f8-e6a2-0410-8959-474133d486e0

--- src/quant/quant_h263.h
+++ src/quant/quant_h263.h
@@ -1,9 +1,47 @@
+/*****************************************************************************
+ *
+ *  XVID MPEG-4 VIDEO CODEC
+ *  - h263 quantization header -
+ *
+ *  Copyright(C) 2002 Michael Militzer
+ *
+ *  This program is an implementation of a part of one or more MPEG-4
+ *  Video tools as specified in ISO/IEC 14496-2 standard.  Those intending
+ *  to use this software module in hardware or software products are
+ *  advised that its use may infringe existing patents or copyrights, and
+ *  any such use would be at such party's own risk.  The original
+ *  developer of this software module and his/her company, and subsequent
+ *  editors and their companies, will have no liability for use of this
+ *  software or modifications or derivatives thereof.
+ *
+ *  This program is free software ; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation ; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY ; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program ; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ *  $Id: quant_h263.h,v 1.7 2002-09-07 10:26:10 edgomez Exp $
+ *
+ ****************************************************************************/
+
 #ifndef _QUANT_H263_H_
 #define _QUANT_H263_H_
 
 #include "../portab.h"
 
-// intra
+/*****************************************************************************
+ * Prototypes
+ ****************************************************************************/
+
+/* intra */
 typedef void (quanth263_intraFunc) (int16_t * coeff,
 									const int16_t * data,
 									const uint32_t quant,
@@ -25,7 +63,7 @@ quanth263_intraFunc dequant_intra_xmm;
 quanth263_intraFunc dequant_intra_sse2;
 quanth263_intraFunc dequant_intra_ia64;
 
-// inter_quant
+/* inter_quant */
 typedef uint32_t(quanth263_interFunc) (int16_t * coeff,
 									   const int16_t * data,
 									   const uint32_t quant);
@@ -39,7 +77,7 @@ quanth263_interFunc quant_inter_mmx;
 quanth263_interFunc quant_inter_sse2;
 quanth263_interFunc quant_inter_ia64;
 
-//inter_dequant
+/*inter_dequant */
 typedef void (dequanth263_interFunc) (int16_t * coeff,
 									  const int16_t * data,
 									  const uint32_t quant);
@@ -54,4 +92,4 @@ dequanth263_interFunc dequant_inter_xmm;
 dequanth263_interFunc dequant_inter_sse2;
 dequanth263_interFunc dequant_inter_ia64;
 
-#endif							/* _QUANT_H263_H_ */
+#endif /* _QUANT_H263_H_ */
