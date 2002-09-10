1035fc3d29e7b6d11d281abd579814b20ef1923c
edgomez <edgomez@060a26f8-e6a2-0410-8959-474133d486e0>
2002-09-10 22:29:18 +0000

    - Legal header - Cosmetic
    git-svn-id: http://svn.xvid.org/trunk/xvidcore@467 060a26f8-e6a2-0410-8959-474133d486e0

--- src/bitstream/cbp.c
+++ src/bitstream/cbp.c
@@ -1,7 +1,49 @@
+/*****************************************************************************
+ *
+ *  XVID MPEG-4 VIDEO CODEC
+ *  - cbp function (zero block flags) -
+ *
+ *  Copyright (C) 2001-2002 - Peter Ross <pross@cs.rmit.edu.au>
+ *
+ *  This program is an implementation of a part of one or more MPEG-4
+ *  Video tools as specified in ISO/IEC 14496-2 standard.  Those intending
+ *  to use this software module in hardware or software products are
+ *  advised that its use may infringe existing patents or copyrights, and
+ *  any such use would be at such party's own risk.  The original
+ *  developer of this software module and his/her company, and subsequent
+ *  editors and their companies, will have no liability for use of this
+ *  software or modifications or derivatives thereof.
+ *
+ *  This program is free software ; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation ; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY ; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program ; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ * $Id: cbp.c,v 1.6 2002-09-10 22:29:18 edgomez Exp $
+ *
+ ****************************************************************************/
+
 #include "../portab.h"
 #include "cbp.h"
 
+/*****************************************************************************
+ * Global function pointer
+ ****************************************************************************/
+
 cbpFuncPtr calc_cbp;
+
+/*****************************************************************************
+ * Functions
+ ****************************************************************************/
 
 /*
  * Returns a field of bits that indicates non zero ac blocks
