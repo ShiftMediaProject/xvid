eace210ad7eff51c4ccf4c048f5043bf27b12894
edgomez <edgomez@060a26f8-e6a2-0410-8959-474133d486e0>
2002-09-12 19:43:14 +0000

    (split)Was only used by bframe encoder
    git-svn-id: http://svn.xvid.org/trunk@481 060a26f8-e6a2-0410-8959-474133d486e0

--- src/image/font.h
+++
@@ -1,8 +0,0 @@
-#ifndef _XVID_FONT_H_
-#define _XVID_FONT_H_
-
-#include "image.h"
-
-void image_printf(IMAGE * img, int edged_width, int height, int x, int y, char *fmt, ...);
-
-#endif /* _XVID_FONT_H_ */
