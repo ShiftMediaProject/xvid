b5f566b448b2881ae5d92d86c6c7622920776efa
edgomez <edgomez@060a26f8-e6a2-0410-8959-474133d486e0>
2002-11-16 23:38:16 +0000

    (split)License Change
    git-svn-id: http://svn.xvid.org/trunk@648 060a26f8-e6a2-0410-8959-474133d486e0

--- src/decoder.c
+++ src/decoder.c
@@ -6,17 +6,10 @@
  *  Copyright(C) 2002 MinChen <chenm001@163.com>
  *               2002 Peter Ross <pross@xvid.org>
  *
- *  This program is an implementation of a part of one or more MPEG-4
- *  Video tools as specified in ISO/IEC 14496-2 standard.  Those intending
- *  to use this software module in hardware or software products are
- *  advised that its use may infringe existing patents or copyrights, and
- *  any such use would be at such party's own risk.  The original
- *  developer of this software module and his/her company, and subsequent
- *  editors and their companies, will have no liability for use of this
- *  software or modifications or derivatives thereof.
+ *  This file is part of XviD, a free MPEG-4 video encoder/decoder
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
+ *  XviD is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
  *
@@ -29,36 +22,36 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  *
- *************************************************************************/
-
-/**************************************************************************
+ *  Under section 8 of the GNU General Public License, the copyright
+ *  holders of XVID explicitly forbid distribution in the following
+ *  countries:
  *
- *  History:
+ *    - Japan
+ *    - United States of America
  *
- *  15.07.2002  fix a bug in B-frame decode at DIRECT mode
-
- *  10.07.2002  added BFRAMES_DEC_DEBUG support
- *              Fix a little bug for low_delay flage
- *              MinChen <chenm001@163.com>
- *  28.06.2002  added basic resync support to iframe/pframe_decode()
- *  22.06.2002	added primative N_VOP support
- *				#define BFRAMES_DEC now enables Minchen's bframe decoder
- *  08.05.2002  add low_delay support for B_VOP decode
- *              MinChen <chenm001@163.com>
- *  05.05.2002  fix some B-frame decode problem
- *  02.05.2002  add B-frame decode support(have some problem);
- *              MinChen <chenm001@163.com>
- *  22.04.2002  add some B-frame decode support;  chenm001 <chenm001@163.com>
- *  29.03.2002  interlacing fix - compensated block wasn't being used when
- *              reconstructing blocks, thus artifacts
- *              interlacing speedup - used transfers to re-interlace
- *              interlaced decoding should be as fast as progressive now
- *  26.03.2002  interlacing support - moved transfers outside decode loop
- *  26.12.2001  decoder_mbinter: dequant/idct moved within if(coded) block
- *  22.12.2001  lock based interpolation
- *  01.12.2001  inital version; (c)2001 peter ross <pross@xvid.org>
+ *  Linking XviD statically or dynamically with other modules is making a
+ *  combined work based on XviD.  Thus, the terms and conditions of the
+ *  GNU General Public License cover the whole combination.
  *
- *  $Id: decoder.c,v 1.42 2002-10-19 12:20:33 edgomez Exp $
+ *  As a special exception, the copyright holders of XviD give you
+ *  permission to link XviD with independent modules that communicate with
+ *  XviD solely through the VFW1.1 and DShow interfaces, regardless of the
+ *  license terms of these independent modules, and to copy and distribute
+ *  the resulting combined work under terms of your choice, provided that
+ *  every copy of the combined work is accompanied by a complete copy of
+ *  the source code of XviD (the version of XviD used to produce the
+ *  combined work), being distributed under the terms of the GNU General
+ *  Public License plus this exception.  An independent module is a module
+ *  which is not derived from or based on XviD.
+ *
+ *  Note that people who make modified versions of XviD are not obligated
+ *  to grant this special exception for their modified versions; it is
+ *  their choice whether to do so.  The GNU General Public License gives
+ *  permission to release a modified version without this exception; this
+ *  exception also makes it possible to release a modified version which
+ *  carries forward this exception.
+ *
+ * $Id: decoder.c,v 1.43 2002-11-16 23:38:15 edgomez Exp $
  *
  *************************************************************************/
 
