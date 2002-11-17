0ae8eaffee2a8ca1012b46e128a9301d559d1759
edgomez <edgomez@060a26f8-e6a2-0410-8959-474133d486e0>
2002-11-17 00:57:58 +0000

    (split)License changed
    git-svn-id: http://svn.xvid.org/trunk@655 060a26f8-e6a2-0410-8959-474133d486e0

--- src/bitstream/ppc_asm/cbp_ppc.s
+++ src/bitstream/ppc_asm/cbp_ppc.s
@@ -1,24 +1,55 @@
 #    Copyright (C) 2002 Guillaume Morin <guillaume@morinfr.org>, Alcôve
 #
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
+# *  This file is part of XviD, a free MPEG-4 video encoder/decoder
+# *
+# *  XviD is free software; you can redistribute it and/or modify it
+# *  under the terms of the GNU General Public License as published by
+# *  the Free Software Foundation; either version 2 of the License, or
+# *  (at your option) any later version.
+# *
+# *  This program is distributed in the hope that it will be useful,
+# *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+# *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# *  GNU General Public License for more details.
+# *
+# *  You should have received a copy of the GNU General Public License
+# *  along with this program; if not, write to the Free Software
+# *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+# *
+# *  Under section 8 of the GNU General Public License, the copyright
+# *  holders of XVID explicitly forbid distribution in the following
+# *  countries:
+# *
+# *    - Japan
+# *    - United States of America
+# *
+# *  Linking XviD statically or dynamically with other modules is making a
+# *  combined work based on XviD.  Thus, the terms and conditions of the
+# *  GNU General Public License cover the whole combination.
+# *
+# *  As a special exception, the copyright holders of XviD give you
+# *  permission to link XviD with independent modules that communicate with
+# *  XviD solely through the VFW1.1 and DShow interfaces, regardless of the
+# *  license terms of these independent modules, and to copy and distribute
+# *  the resulting combined work under terms of your choice, provided that
+# *  every copy of the combined work is accompanied by a complete copy of
+# *  the source code of XviD (the version of XviD used to produce the
+# *  combined work), being distributed under the terms of the GNU General
+# *  Public License plus this exception.  An independent module is a module
+# *  which is not derived from or based on XviD.
+# *
+# *  Note that people who make modified versions of XviD are not obligated
+# *  to grant this special exception for their modified versions; it is
+# *  their choice whether to do so.  The GNU General Public License gives
+# *  permission to release a modified version without this exception; this
+# *  exception also makes it possible to release a modified version which
+# *  carries forward this exception.
+# *
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program; if not, write to the Free Software
-#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-#
-#    $Id: cbp_ppc.s,v 1.9 2002-03-28 12:29:58 canard Exp $
+#    $Id: cbp_ppc.s,v 1.10 2002-11-17 00:57:57 edgomez Exp $
 #    $Source: /home/xvid/cvs_copy/cvs-server-root/xvid/xvidcore/src/bitstream/ppc_asm/cbp_ppc.s,v $
-#    $Date: 2002-03-28 12:29:58 $
-#    $Author: canard $
+#    $Date: 2002-11-17 00:57:57 $
+#    $Author: edgomez $
 #
 #    This is my first PPC ASM attempt. So I might do nasty things.
 #    Please send any comments to <guillaume@morinfr.org>
