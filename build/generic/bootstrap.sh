399bfb5af43a03ddfe3e305ace69b59c2693e3e0
edgomez <edgomez@060a26f8-e6a2-0410-8959-474133d486e0>
2003-02-20 23:40:48 +0000

    Fix 'which' output dependency. If it is still not right, then we'll have to find another solution.
    git-svn-id: http://svn.xvid.org/trunk/xvidcore@880 060a26f8-e6a2-0410-8959-474133d486e0

--- build/generic/bootstrap.sh
+++ build/generic/bootstrap.sh
@@ -3,42 +3,36 @@
 # This file builds the configure script and copies all needed files
 # provided by automake/libtoolize
 #
-# $Id: bootstrap.sh,v 1.2 2003-02-20 18:39:23 edgomez Exp $
+# $Id: bootstrap.sh,v 1.3 2003-02-20 23:40:48 edgomez Exp $
 
 
 ##############################################################################
 # Detect the right autoconf script
 ##############################################################################
 
-# we first test Debian GNU/Linux autoconf for 2.50 series
+# Find a suitable autoconf
 AUTOCONF=`which autoconf2.50`
-
-if [ ! -x "$AUTOCONF" ] ; then
-
-    # Test failed, testing generic name -- many distros are shipping 2.13 with
-    # that name, that's why we have to perform an additional test for version.
+if [ $? -ne 0 ] ; then
     AUTOCONF=`which autoconf`
-
-    if [ ! -x "$AUTOCONF" ] ; then
-	echo "No autoconf binary found in PATH"
-	exit -1
+    if [ $? -ne 0 ] ; then
+        echo "Autoconf not found"
+        exit -1
     fi
+fi
 
-    # Tests the autoconf version
-    AC_VER=`$AUTOCONF --version | head -1 | sed 's/^[^0-9]*//i'`
-    AC_MAJORVER=`echo $AC_VER | cut -f1 -d'.'`
-    AC_MINORVER=`echo $AC_VER | cut -f2 -d'.'`
+# Tests the autoconf version
+AC_VER=`$AUTOCONF --version | head -1 | sed 's/'^[^0-9]*'/''/'`
+AC_MAJORVER=`echo $AC_VER | cut -f1 -d'.'`
+AC_MINORVER=`echo $AC_VER | cut -f2 -d'.'`
 
-    if [ "$AC_MAJORVER" -lt "2" ]; then
-	echo "This bootstrapper needs Autoconf >= 2.50 (detected $AC_VER)"
-	exit -1
-    fi
+if [ "$AC_MAJORVER" -lt "2" ]; then
+    echo "This bootstrapper needs Autoconf >= 2.50 (detected $AC_VER)"
+    exit -1
+fi
 
-    if [ "$AC_MINORVER" -lt "50" ]; then
-	echo "This bootstrapper needs Autoconf >= 2.50 (detected $AC_VER)"
-	exit -1
-    fi
-
+if [ "$AC_MINORVER" -lt "50" ]; then
+    echo "This bootstrapper needs Autoconf >= 2.50 (detected $AC_VER)"
+    exit -1
 fi
 
 ##############################################################################
