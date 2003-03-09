479e24229c0541deca5f0061f3c94daca4da7463
chl <chl@060a26f8-e6a2-0410-8959-474133d486e0>
2003-03-09 11:30:24 +0000

    Speedup for calc_cbp_c(), loop unrolling / 64bit integers
    git-svn-id: http://svn.xvid.org/trunk/xvidcore@911 060a26f8-e6a2-0410-8959-474133d486e0

--- src/bitstream/cbp.c
+++ src/bitstream/cbp.c
@@ -7,26 +7,80 @@ cbpFuncPtr calc_cbp;
  * Returns a field of bits that indicates non zero ac blocks
  * for this macro block
  */
+
+/* naive C */
 uint32_t
-calc_cbp_c(const int16_t codes[6 * 64])
+calc_cbp_plain(const int16_t codes[6 * 64])
 {
-	uint32_t i, j;
+	int i, j;
 	uint32_t cbp = 0;
 
 	for (i = 0; i < 6; i++) {
-		for (j = 1; j < 61; j += 4) {
-			if (codes[i * 64 + j] | codes[i * 64 + j + 1] |
-				codes[i * 64 + j + 2] | codes[i * 64 + j + 3]) {
-				cbp |= 1 << (5 - i);
+		for (j=1; j<64;j++) {
+			if (codes[64*i+j]) {
+            	cbp |= 1 << (5-i);
 				break;
 			}
 		}
+	}
+	return cbp;
+}
 
-		if (codes[i * 64 + j] | codes[i * 64 + j + 1] | codes[i * 64 + j + 2])
-			cbp |= 1 << (5 - i);
+/* optimized C */
+uint32_t
+calc_cbp_c(const int16_t codes[6 * 64])
+{
+	int i, j;
+	uint32_t cbp = 0;
+/* if definition is changed (e.g. from int16_t to something like int) this routine 
+   is not possible anymore! */
 
+   for (i = 5; i >= 0; i--, codes += 64) {
+
+		uint64_t *codes64 = (uint64_t*)codes;
+		cbp += cbp; 
+        if (codes[1] || codes[2] || codes[3]) {
+			cbp++;
+			continue;
+		}
+        if (codes64[1] | codes64[2] | codes64[3]) {
+			cbp++;
+			continue;
+		}
+        if (codes64[4] | codes64[5] | codes64[6] | codes64[7]) {
+			cbp++;
+			continue;
+		}
+        if (codes64[8] | codes64[9] | codes64[10] | codes64[11]) {
+			cbp++;
+			continue;
+		}
+        if (codes64[12] | codes64[13] | codes64[14] | codes64[15]) {
+			cbp++;
+			continue;
+		}
+    }
+
+	return cbp;
+}
+
+
+
+
+/* older code maybe better on some plattforms? */
+#if (0==1)
+	for (i = 5; i >= 0; i--) {
+		if (codes[1] | codes[2] | codes[3])
+                        cbp |= 1 << i;
+		else {
+			for (j = 4; j <= 56; j+=4) 	/* [60],[61],[62],[63] are last */
+				if (codes[j] | codes[j+1] | codes[j+2] | codes[j+3]) {
+				cbp |= 1 << i;
+				break;
+			}
+		}
+		codes += 64;
 	}
 
 	return cbp;
-
-}
+#endif
