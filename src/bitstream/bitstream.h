b826b1acd646efdfa773b88716cfafa64d37fcbd
edgomez <edgomez@060a26f8-e6a2-0410-8959-474133d486e0>
2003-04-04 22:16:15 +0000

    (split)Fixes 32 bit misaligned reads on ARM (+ some sync work with old 0.9.x tree for cleanups)
    git-svn-id: http://svn.xvid.org/trunk@972 060a26f8-e6a2-0410-8959-474133d486e0

--- src/bitstream/bitstream.h
+++ src/bitstream/bitstream.h
@@ -1,54 +1,29 @@
- /******************************************************************************
-  *                                                                            *
-  *  This file is part of XviD, a free MPEG-4 video encoder/decoder            *
-  *                                                                            *
-  *  XviD is an implementation of a part of one or more MPEG-4 Video tools     *
-  *  as specified in ISO/IEC 14496-2 standard.  Those intending to use this    *
-  *  software module in hardware or software products are advised that its     *
-  *  use may infringe existing patents or copyrights, and any such use         *
-  *  would be at such party's own risk.  The original developer of this        *
-  *  software module and his/her company, and subsequent editors and their     *
-  *  companies, will have no liability for use of this software or             *
-  *  modifications or derivatives thereof.                                     *
-  *                                                                            *
-  *  XviD is free software; you can redistribute it and/or modify it           *
-  *  under the terms of the GNU General Public License as published by         *
-  *  the Free Software Foundation; either version 2 of the License, or         *
-  *  (at your option) any later version.                                       *
-  *                                                                            *
-  *  XviD is distributed in the hope that it will be useful, but               *
-  *  WITHOUT ANY WARRANTY; without even the implied warranty of                *
-  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
-  *  GNU General Public License for more details.                              *
-  *                                                                            *
-  *  You should have received a copy of the GNU General Public License         *
-  *  along with this program; if not, write to the Free Software               *
-  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA  *
-  *                                                                            *
-  ******************************************************************************/
-
- /******************************************************************************
-  *                                                                            *
-  *  bitstream.h                                                               *
-  *                                                                            *
-  *  Copyright (C) 2001 - Peter Ross <pross@cs.rmit.edu.au>                    *
-  *                                                                            *
-  *  For more information visit the XviD homepage: http://www.xvid.org         *
-  *                                                                            *
-  ******************************************************************************/
-
- /******************************************************************************
-  *																			   *	
-  *  Revision history:                                                         *
-  *                                                                            *
-  *  28.06.2002 addded BitstreamNumBitsToByteAlign()                           *
-  *                    BitstreamShowBitsFromByteAlign()                        *
-  *  26.03.2002 interlacing support - modified putvol/vopheaders paramters     *
-  *  04.03.2002 putbits speedup (Isibaar)                                      *
-  *  03.03.2002 merged BITREADER and BITWRITER (Isibaar)                       *
-  *	 16.12.2001	inital version                                           	   *
-  *																			   *
-  ******************************************************************************/
+/*****************************************************************************
+ *
+ *  XVID MPEG-4 VIDEO CODEC
+ *  - Bitstream reader/writer inlined functions and constants-
+ *
+ *  Copyright (C) 2001-2003 - Peter Ross <pross@xvid.org>
+ *
+ *  This file is part of XviD, a free MPEG-4 video encoder/decoder
+ *
+ *  XviD is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+ *
+ * $Id: bitstream.h,v 1.18 2003-04-04 22:16:15 edgomez Exp $
+ *
+ ****************************************************************************/
 
 #ifndef _BITSTREAM_H_
 #define _BITSTREAM_H_
@@ -57,9 +32,12 @@
 #include "../decoder.h"
 #include "../encoder.h"
 
-// comment any #defs we dont use
 
-/* start codes */
+/*****************************************************************************
+ * Constants
+ ****************************************************************************/
+
+/* comment any #defs we dont use */
 
 #define VIDOBJ_START_CODE		0x00000100	/* ..0x0000011f  */
 #define VIDOBJLAY_START_CODE	0x00000120	/* ..0x0000012f */
@@ -67,39 +45,39 @@
 #define VISOBJSEQ_STOP_CODE		0x000001b1	/* ??? */
 #define USERDATA_START_CODE		0x000001b2
 #define GRPOFVOP_START_CODE		0x000001b3
-//#define VIDSESERR_ERROR_CODE  0x000001b4
+/*#define VIDSESERR_ERROR_CODE  0x000001b4 */
 #define VISOBJ_START_CODE		0x000001b5
 #define VOP_START_CODE			0x000001b6
-//#define STUFFING_START_CODE	0x000001c3
+/*#define STUFFING_START_CODE	0x000001c3 */
 
 
 #define VISOBJ_TYPE_VIDEO				1
-//#define VISOBJ_TYPE_STILLTEXTURE      2
-//#define VISOBJ_TYPE_MESH              3
-//#define VISOBJ_TYPE_FBA               4
-//#define VISOBJ_TYPE_3DMESH            5
+/*#define VISOBJ_TYPE_STILLTEXTURE      2 */
+/*#define VISOBJ_TYPE_MESH              3 */
+/*#define VISOBJ_TYPE_FBA               4 */
+/*#define VISOBJ_TYPE_3DMESH            5 */
 
 
 #define VIDOBJLAY_TYPE_SIMPLE			1
-//#define VIDOBJLAY_TYPE_SIMPLE_SCALABLE    2
+/*#define VIDOBJLAY_TYPE_SIMPLE_SCALABLE    2 */
 #define VIDOBJLAY_TYPE_CORE				3
 #define VIDOBJLAY_TYPE_MAIN				4
-//#define VIDOBJLAY_TYPE_NBIT				5
-//#define VIDOBJLAY_TYPE_ANIM_TEXT			6
-//#define VIDOBJLAY_TYPE_ANIM_MESH			7
-//#define VIDOBJLAY_TYPE_SIMPLE_FACE		8
-//#define VIDOBJLAY_TYPE_STILL_SCALABLE		9
+/*#define VIDOBJLAY_TYPE_NBIT				5 */
+/*#define VIDOBJLAY_TYPE_ANIM_TEXT			6 */
+/*#define VIDOBJLAY_TYPE_ANIM_MESH			7 */
+/*#define VIDOBJLAY_TYPE_SIMPLE_FACE		8 */
+/*#define VIDOBJLAY_TYPE_STILL_SCALABLE		9 */
 #define VIDOBJLAY_TYPE_ART_SIMPLE		10
-//#define VIDOBJLAY_TYPE_CORE_SCALABLE		11
+/*#define VIDOBJLAY_TYPE_CORE_SCALABLE		11 */
 #define VIDOBJLAY_TYPE_ACE				12
-//#define VIDOBJLAY_TYPE_SIMPLE_FBA			13
+/*#define VIDOBJLAY_TYPE_SIMPLE_FBA			13 */
 
 
-//#define VIDOBJLAY_AR_SQUARE           1
-//#define VIDOBJLAY_AR_625TYPE_43       2
-//#define VIDOBJLAY_AR_525TYPE_43       3
-//#define VIDOBJLAY_AR_625TYPE_169      8
-//#define VIDOBJLAY_AR_525TYPE_169      9
+/*#define VIDOBJLAY_AR_SQUARE           1 */
+/*#define VIDOBJLAY_AR_625TYPE_43       2 */
+/*#define VIDOBJLAY_AR_525TYPE_43       3 */
+/*#define VIDOBJLAY_AR_625TYPE_169      8 */
+/*#define VIDOBJLAY_AR_525TYPE_169      9 */
 #define VIDOBJLAY_AR_EXTPAR				15
 
 
@@ -118,29 +96,32 @@
 #define READ_MARKER()	BitstreamSkip(bs, 1)
 #define WRITE_MARKER()	BitstreamPutBit(bs, 1)
 
-// vop coding types 
-// intra, prediction, backward, sprite, not_coded
+/* vop coding types  */
+/* intra, prediction, backward, sprite, not_coded */
 #define I_VOP	0
 #define P_VOP	1
 #define B_VOP	2
 #define S_VOP	3
 #define N_VOP	4
 
-// resync-specific
+/* resync-specific */
 #define NUMBITS_VP_RESYNC_MARKER  17
 #define RESYNC_MARKER 1
 
 
+/*****************************************************************************
+ * Prototypes
+ ****************************************************************************/
+
 int read_video_packet_header(Bitstream *bs, 
-						DECODER * dec, 
-						const int addbits, 
-						int * quant, 
-						int * fcode_forward,
-						int  * fcode_backward,
-						int * intra_dc_threshold);
+							 DECODER * dec, 
+							 const int addbits, 
+							 int *quant, 
+							 int *fcode_forward,
+							 int *fcode_backward,
+							 int *intra_dc_threshold);
 
-
-// header stuff
+/* header stuff */
 int BitstreamReadHeaders(Bitstream * bs,
 						 DECODER * dec,
 						 uint32_t * rounding,
@@ -173,23 +154,31 @@ BitstreamInit(Bitstream * const bs,
 			  uint32_t length)
 {
 	uint32_t tmp;
+	size_t bitpos;
+	ptr_t adjbitstream = (ptr_t)bitstream;
 
-	bs->start = bs->tail = (uint32_t *) bitstream;
+	/*
+	 * Start the stream on a uint32_t boundary, by rounding down to the
+	 * previous uint32_t and skipping the intervening bytes.
+	 */
+	bitpos = ((sizeof(uint32_t)-1) & (size_t)bitstream);
+	adjbitstream = adjbitstream - bitpos;
+	bs->start = bs->tail = (uint32_t *) adjbitstream;
 
-	tmp = *(uint32_t *) bitstream;
+	tmp = *bs->start;
 #ifndef ARCH_IS_BIG_ENDIAN
 	BSWAP(tmp);
 #endif
 	bs->bufa = tmp;
 
-	tmp = *((uint32_t *) bitstream + 1);
+	tmp = *(bs->start + 1);
 #ifndef ARCH_IS_BIG_ENDIAN
 	BSWAP(tmp);
 #endif
 	bs->bufb = tmp;
 
 	bs->buf = 0;
-	bs->pos = 0;
+	bs->pos = bs->initpos = bitpos*8;
 	bs->length = length;
 }
 
@@ -216,7 +205,7 @@ BitstreamReset(Bitstream * const bs)
 	bs->bufb = tmp;
 
 	bs->buf = 0;
-	bs->pos = 0;
+	bs->pos = bs->initpos;
 }
 
 
@@ -261,7 +250,7 @@ BitstreamSkip(Bitstream * const bs,
 }
 
 
-// number of bits to next byte alignment
+/* number of bits to next byte alignment */
 static __inline uint32_t 
 BitstreamNumBitsToByteAlign(Bitstream *bs)
 {
@@ -270,7 +259,7 @@ BitstreamNumBitsToByteAlign(Bitstream *bs)
 }
 
 
-// show nbits from next byte alignment
+/* show nbits from next byte alignment */
 static __inline uint32_t
 BitstreamShowBitsFromByteAlign(Bitstream *bs, int bits)
 {
@@ -309,18 +298,19 @@ BitstreamByteAlign(Bitstream * const bs)
 static uint32_t __inline
 BitstreamPos(const Bitstream * const bs)
 {
-	return 8 * ((ptr_t)bs->tail - (ptr_t)bs->start) + bs->pos;
+	return((uint32_t)(8*((ptr_t)bs->tail - (ptr_t)bs->start) + bs->pos - bs->initpos));
 }
 
 
-/*	flush the bitstream & return length (unit bytes)
-	NOTE: assumes no futher bitstream functions will be called.
+/*
+ * flush the bitstream & return length (unit bytes)
+ * NOTE: assumes no futher bitstream functions will be called.
  */
 
 static uint32_t __inline
 BitstreamLength(Bitstream * const bs)
 {
-	uint32_t len = (ptr_t) bs->tail - (ptr_t) bs->start;
+	uint32_t len = (uint32_t)((ptr_t)bs->tail - (ptr_t)bs->start);
 
 	if (bs->pos) {
 		uint32_t b = bs->buf;
@@ -332,6 +322,10 @@ BitstreamLength(Bitstream * const bs)
 
 		len += (bs->pos + 7) / 8;
 	}
+
+	/* initpos is always on a byte boundary */
+	if (bs->initpos)
+		len -= bs->initpos/8;
 
 	return len;
 }
@@ -356,7 +350,6 @@ BitstreamForward(Bitstream * const bs,
 		bs->pos -= 32;
 	}
 }
-
 
 /* read n bits from bitstream */
 
@@ -420,7 +413,6 @@ BitstreamPutBits(Bitstream * const bs,
 	}
 }
 
-
 static const int stuffing_codes[8] =
 {
 	        /* nbits     stuffing code */
@@ -441,14 +433,14 @@ BitstreamPad(Bitstream * const bs)
 {
 	int bits = 8 - (bs->pos % 8);
 	if (bits < 8)
-	{
 		BitstreamPutBits(bs, stuffing_codes[bits - 1], bits);
-	}
 }
 
 
-/* pad bitstream to the next byte boundary 
-   alway pad: even if currently at the byte boundary */
+/*
+ * pad bitstream to the next byte boundary 
+ * alway pad: even if currently at the byte boundary
+ */
 
 static void __inline
 BitstreamPadAlways(Bitstream * const bs)
@@ -457,5 +449,4 @@ BitstreamPadAlways(Bitstream * const bs)
 	BitstreamPutBits(bs, stuffing_codes[bits - 1], bits);
 }
 
-
-#endif							/* _BITSTREAM_H_ */
+#endif /* _BITSTREAM_H_ */
