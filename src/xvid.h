376bae9237c0d9e4009b81f83a88a7f267bbdc51
edgomez <edgomez@060a26f8-e6a2-0410-8959-474133d486e0>
2004-08-29 11:57:35 +0000

    (split)Merging with 1.0 tree
    git-svn-id: http://svn.xvid.org/trunk@1545 060a26f8-e6a2-0410-8959-474133d486e0

--- src/xvid.h
+++ src/xvid.h
@@ -19,7 +19,7 @@
  *  along with this program ; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  *
- * $Id: xvid.h,v 1.44 2004-07-18 15:24:05 edgomez Exp $
+ * $Id: xvid.h,v 1.45 2004-08-29 11:57:35 edgomez Exp $
  *
  ****************************************************************************/
 
@@ -73,7 +73,7 @@ extern "C" {
  * doesnt hurt but not increasing it could cause difficulty for decoders in the
  * future
  */
-#define XVID_BS_VERSION 35
+#define XVID_BS_VERSION 36
 
 /*****************************************************************************
  * error codes
