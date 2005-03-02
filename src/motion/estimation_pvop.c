f65311597756ef657d87f1fb8a9eb90ffddbef2c
Isibaar <Isibaar@060a26f8-e6a2-0410-8959-474133d486e0>
2005-03-02 20:22:10 +0000

    (split)- Cartoon mode bugfix
    git-svn-id: http://svn.xvid.org/trunk@1601 060a26f8-e6a2-0410-8959-474133d486e0

--- src/motion/estimation_pvop.c
+++ src/motion/estimation_pvop.c
@@ -21,7 +21,7 @@
  *  along with this program ; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  *
- * $Id: estimation_pvop.c,v 1.12 2005-02-20 13:12:26 syskin Exp $
+ * $Id: estimation_pvop.c,v 1.13 2005-03-02 20:22:10 Isibaar Exp $
  *
  ****************************************************************************/
 
@@ -880,6 +880,7 @@ MotionEstimation(MBParam * const pParam,
 			if ((current->vop_flags & XVID_VOP_CARTOON) &&
 				((sad00 < pMB->quant * 4 * skip_thresh) || (sad00 < stat_thresh))) {
 				ZeroMacroblockP(pMB, sad00);
+				pMB->cbp = 0x2f;
 				continue;
 			}
 
