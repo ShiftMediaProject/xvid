1a8f9dccf5a6a53324774d5416bbbd0b0a4a4247
syskin <syskin@060a26f8-e6a2-0410-8959-474133d486e0>
2006-05-06 04:37:15 +0000

    missing #include b0rks compilation
    git-svn-id: http://svn.xvid.org/trunk/xvidcore@1704 060a26f8-e6a2-0410-8959-474133d486e0

--- src/plugins/plugin_lumimasking.c
+++ src/plugins/plugin_lumimasking.c
@@ -20,13 +20,14 @@
  *  along with this program ; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  *
- * $Id: plugin_lumimasking.c,v 1.5 2006-04-15 04:17:02 syskin Exp $
+ * $Id: plugin_lumimasking.c,v 1.6 2006-05-06 04:37:15 syskin Exp $
  *
  ****************************************************************************/
 
 #include <stdlib.h>
 
 #include "../xvid.h"
+#include "../global.h"
 #include "../portab.h"
 #include "../utils/emms.h"
 
