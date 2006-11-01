3f636e2b742ae2a75445d138216fae63dccb58f9
Isibaar <Isibaar@060a26f8-e6a2-0410-8959-474133d486e0>
2006-11-01 10:04:29 +0000

    (split)- upped BS_VERSION to 47
    git-svn-id: http://svn.xvid.org/trunk@1750 060a26f8-e6a2-0410-8959-474133d486e0

--- src/xvid.h
+++ src/xvid.h
@@ -19,7 +19,7 @@
  *  along with this program ; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  *
- * $Id: xvid.h,v 1.57 2006-10-30 11:21:42 Skal Exp $
+ * $Id: xvid.h,v 1.58 2006-11-01 10:04:29 Isibaar Exp $
  *
  ****************************************************************************/
 
@@ -73,7 +73,7 @@ extern "C" {
  * doesnt hurt but not increasing it could cause difficulty for decoders in the
  * future
  */
-#define XVID_BS_VERSION 45
+#define XVID_BS_VERSION 47
 
 /*****************************************************************************
  * error codes
