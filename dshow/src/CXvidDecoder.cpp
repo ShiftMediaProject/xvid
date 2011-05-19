f4d1128ae8c8950930d8e1f997f15555ef8edf8e
Isibaar <Isibaar@060a26f8-e6a2-0410-8959-474133d486e0>
2011-05-19 12:48:59 +0000

    autoprops property set also for .cpp files
    git-svn-id: http://svn.xvid.org/branches/release-1_3-branch/xvidcore@2006 060a26f8-e6a2-0410-8959-474133d486e0

--- dshow/src/CXvidDecoder.cpp
+++ dshow/src/CXvidDecoder.cpp
@@ -20,7 +20,7 @@
  *  along with this program ; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
  *
- * $Id: CXvidDecoder.cpp,v 1.25.2.4 2011-03-17 15:13:25 Isibaar Exp $
+ * $Id$
  *
  ****************************************************************************/
 
