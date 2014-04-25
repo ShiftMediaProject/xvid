fabbad2009227cc53e8f306b4b9eaf7f4af24c3c
Isibaar <Isibaar@060a26f8-e6a2-0410-8959-474133d486e0>
2014-04-25 15:38:57 +0000

    Increased bs number
    git-svn-id: http://svn.xvid.org/branches/release-1_3-branch@2086 060a26f8-e6a2-0410-8959-474133d486e0

--- src/xvid.h
+++ src/xvid.h
@@ -71,7 +71,7 @@ extern "C" {
  * doesnt hurt but not increasing it could cause difficulty for decoders in the
  * future
  */
-#define XVID_BS_VERSION 65 
+#define XVID_BS_VERSION 66 
 
 /*****************************************************************************
  * error codes
